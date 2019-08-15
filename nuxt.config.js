0de403fb14a6a5fb85cb0a262cfa7f55d7fdc8a0
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-15 21:09:42 +0200

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,24 +5,37 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Schoolsyst' || process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A complete, centralized school management system made for students, by a student.' || process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto+Mono'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined'},
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.7/lottie.min.js' },
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { 
+    color: '#000',
+    height: '10px'
+  },
   /*
   ** Global CSS
   */
   css: [
+    '@/assets/reset.sass',
+    '@/assets/defaults.sass',
+    // Fonts
+    '@/assets/fonts/Manrope/import.css',
+
   ],
   /*
   ** Plugins to load before mounting the App
@@ -41,12 +54,27 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
+    // '@nuxtjs/auth',
   ],
+  /*
+  ** Auth module configuration.
+  ** See https://auth.nuxtjs.org
+  */
+  // auth: {
+  //   strategies: {
+  //     local: {
+  //       endpoints: {
+  //         login: { url: '/api/auth/login', method: 'post', propertyName: '' }
+  //       }
+  //     }
+  //   }
+  // },
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    // baseURL: 'http://127.0.0.1:8000/api/'
   },
   /*
   ** Build configuration
@@ -57,5 +85,8 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
+  // router: {
+  //   middleware: ['auth']
+  // },
 }
