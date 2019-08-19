4e8384f246cb1afa8ee1e6ea8af8c084518347c7
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-19 15:44:55 +0200

    subject badge & dot components

--- components/BadgeSubject.vue
+++ components/BadgeSubject.vue
@@ -1,44 +1,20 @@
-<template>
-    <div class="BadgeSubject" :style="{backgroundColor: color}">
-        <span class="subject-abbr">{{abbreviation.toUpperCase()}}</span>
-    </div>
+<template lang="pug">
+button.BadgeSubject(:style="{backgroundColor: color}") {{abbreviation}}
 </template>
 
 <script>
+
 export default {
     name: 'BadgeSubject',
-
-
     props: {
-        color: {
-            // hex color format
-            validator: function(value) { return value.match(/#(([A-Za-z0-9]{3}){1,2})/) }
-        },
         abbreviation: {
-            // 3-letters string
-            validator: function(value) { return String(value).length === 3 }
+            type:String,
+            default:'...'
+        },
+        color: {
+            type:String,
+            default:'#000000'
         }
-    },
-
-
-    data() {
-        return {
-            
-        }
-    },
-
-    computed: {
-        
-    },
-
-
-    created() {
-
-    },
-
-
-    methods: {
-
     }
 }
 </script>
@@ -46,4 +22,18 @@ export default {
 <style lang="sass" scoped>
 @import '~/assets/defaults'
 
+.BadgeSubject
+    width: 100px
+    height: 50px
+    font-family: 'Roboto Mono', monospace
+    font-size: 30px
+    padding: 10px 15px
+    border-radius: 10px
+    color: #fff
+    display: flex
+    justify-content: center
+    align-items: center
+    text-transform: uppercase
+    &:focus, &:hover
+        opacity: 0.75
 </style>
---
+++ components/SubjectDot.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+button.SubjectDot(:style="{backgroundColor: bgcolor}" @click="$emit('click', $event)")
+  i.icon.material-icons(v-if="color === 'unknown'") more_horiz
+</template>
+
+<script>
+export default {
+  name: "SubjectDot",
+  props: {
+    color: {
+      type: String,
+      default: 'unknown'
+    },
+
+  },
+  computed: {
+    bgcolor() {
+      return this.color === 'unknown' ? 'black' : this.color
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/defaults'
+
+.SubjectDot
+    $size: 40px
+    height: $size
+    width: $size
+    min-width: $size
+    min-height: $size
+    border-radius: $size / 2
+    display: flex
+    justify-content: center
+    align-items: center
+
+.icon
+  color: white
+</style>
