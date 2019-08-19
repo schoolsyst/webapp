24362328ad817900f7d8db4c248aa034be8aecff
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-19 16:20:22 +0200

    prettier formatting (mostly idk)

--- store/schedule.js
+++ store/schedule.js
@@ -10,7 +10,7 @@ import { extendMoment } from "moment-range";
 const moment = extendMoment(Moment);
 
 const ptime = time => moment(time, "HH:mm:ss");
-const pdate = date => moment(date, ["DD/MM/YYYY", "YYYY-MM-DD"])
+const pdate = date => moment(date, ["DD/MM/YYYY", "YYYY-MM-DD"]);
 
 export const state = () => ({
   additions: [],
@@ -30,16 +30,16 @@ export const getters = {
   allDeletions(state) {
     return state.deletions;
   },
-  setting: (state, getters, rootState, rootGetters) => (settingName) => {
-    let settings = rootGetters.allSettings
-    settings = settings.filter(setting => setting.name == settingName)
+  setting: (state, getters, rootState, rootGetters) => settingName => {
+    let settings = rootGetters.allSettings;
+    settings = settings.filter(setting => setting.setting.key == settingName);
     if (!settings.length) {
-      console.error(`Setting "${settingName}" does not exist for this user`)
-      return null
+      console.error(`Setting "${settingName}" does not exist for this user`);
+      return null;
     }
-    return settings[0].value
+    return settings[0].value;
   },
-  trimesterStart: (state, getters, rootState, rootGetters) => (trimester) => {
+  trimesterStart: (state, getters, rootState, rootGetters) => trimester => {
     switch (trimester) {
       case 1:
         return pdate(getters.setting("year_start"));
@@ -59,19 +59,21 @@ export const getters = {
     let start = getters.setting("year_start");
     // convert to week-of-year number, and get if its even or odd.
     // tested date will be [base] (Q1 or Q2) if its also even.
-    let startingWeekIsEven = moment(start).isoWeek() % 2 === 0
-    let other = base === 'Q1' ? 'Q2' : 'Q1'
-    return moment(date).isoWeek() % 2 === 0 && startingWeekIsEven ? base : other
+    let startingWeekIsEven = moment(start).isoWeek() % 2 === 0;
+    let other = base === "Q1" ? "Q2" : "Q1";
+    return moment(date).isoWeek() % 2 === 0 && startingWeekIsEven
+      ? base
+      : other;
   },
 
   currentWeekType: (state, getters) => getters.weekType(moment()),
 
-  coursesIn: (state, getters) => (start, upto, debug=null) => {
+  coursesIn: (state, getters) => (start, upto, debug = null) => {
     // setup vars
     /* if (debug) {
       debugger
     } */
-    
+
     let computedEvents = [];
     let baseEvents = getters.allEvents;
     let deletions = getters.allDeletions;
@@ -80,12 +82,12 @@ export const getters = {
     let weekday, deleted, added, weekType;
     if (baseEvents.length < 1) return [];
 
-    if(debug) console.log(start.format('D/M/Y'), '->', upto.format('D/M/Y'))
+    if (debug) console.log(start.format("D/M/Y"), "->", upto.format("D/M/Y"));
     // inclusive range, since we check for isBefore (and not isBefore && isSame, lot more costly)
     // EDIT: this should turn it into an inclusive range
     //start = start.subtract(1, 'day')
     // loop through every date between start and upto
-    for (let current = start; current.isBefore(upto); current.add(1, 'day')) {
+    for (let current = start; current.isBefore(upto); current.add(1, "day")) {
       //TODO: continue if date in getters.offdays
       weekday = [
         "monday",
@@ -97,35 +99,45 @@ export const getters = {
         "sunday"
       ][current.day()];
 
-      weekType = getters.weekType(current)
-      if(debug) console.log(`${current.format('D/M/Y')}#${weekday}:${weekType}`)
+      weekType = getters.weekType(current);
+      if (debug)
+        console.log(`${current.format("D/M/Y")}#${weekday}:${weekType}`);
       let dayBaseEvents = baseEvents.filter(ev => {
-        return ev.day === weekday && (ev.week_type === 'both' || ev.week_type === weekType)
+        return (
+          ev.day === weekday &&
+          (ev.week_type === "both" || ev.week_type === weekType)
+        );
       });
-      if(debug) console.log(dayBaseEvents.map(e => `${e.subject.abbreviation.toUpperCase()}@${e.start}`))
+      if (debug)
+        console.log(
+          dayBaseEvents.map(
+            e => `${e.subject.abbreviation.toUpperCase()}@${e.start}`
+          )
+        );
 
       // loop through the events of that weekday
       for (const baseEvent of dayBaseEvents) {
         // if we have at least one deletion for this event
         if (deletions.length) {
-          deleted = deletions.filter(deletion => deletion.event.id === baseEvent.id)
-                             .length >= 1;
+          deleted =
+            deletions.filter(deletion => deletion.event.id === baseEvent.id)
+              .length >= 1;
         } else {
           deleted = false;
         }
-        computedEvents.push({ 
+        computedEvents.push({
           ...baseEvent,
-          deleted, 
-          date: current.format('DD/MM/YYYY'),
+          deleted,
+          date: current.format("DD/MM/YYYY"),
           _date: current,
-          added: false,
+          added: false
         });
       }
 
       if (additions.length) {
         // loop thourgh the ADDED events of that weekday
-        for (const addedEvent of additions.filter(
-          event => pdate(event.current).isSame(current)
+        for (const addedEvent of additions.filter(event =>
+          pdate(event.current).isSame(current)
         )) {
           computedEvents.push({
             ...addedEvent,
@@ -141,7 +153,7 @@ export const getters = {
   },
   coursesInCurrentWeek: (state, getters) => {
     let now = moment();
-    return getters.coursesIn(moment().startOf('week'), moment().endOf('week'));
+    return getters.coursesIn(moment().startOf("week"), moment().endOf("week"));
   },
   eventsOf: (state, getters) => subjectSlug => {
     return getters.allEvents.filter(
@@ -187,47 +199,52 @@ export const getters = {
     }
   },
   // note: doesn't get other courses in the same day as currentSubject
-  nextCourseOf: (state, getters) => (subjectSlug) => {
-    const tomorrow = moment().add(1, 'days')
+  nextCourseOf: (state, getters) => subjectSlug => {
+    const tomorrow = moment().add(1, "days");
     // get courses that are after tomorrow
     // FIXME: shouldn't also get courses from today
-    let courses = getters.coursesIn(tomorrow, moment().add(2, 'weeks')) 
-    if (!courses.length) return null
+    let courses = getters.coursesIn(tomorrow, moment().add(2, "weeks"));
+    if (!courses.length) return null;
     // console.log(courses.map(c => c.date.format('D/M/Y')))
     // and match the requested subject
-    courses = courses.filter(course => course.subject.slug === subjectSlug)
-    if (!courses.length) return null
+    courses = courses.filter(course => course.subject.slug === subjectSlug);
+    if (!courses.length) return null;
     // and sort them by ascending date
-    courses = courses.sort((a, b) => pdate(a.date).isAfter(pdate(b.date)) ? 1 : -1)
-    return courses[0]
+    courses = courses.sort((a, b) =>
+      pdate(a.date).isAfter(pdate(b.date)) ? 1 : -1
+    );
+    return courses[0];
   },
   nextCourseOfCurrentSubject: (state, getters) => {
     if (!getters.currentCourse) {
-      return null
+      return null;
     }
-    return getters.nextCourseOf(getters.currentCourse.subject)
+    return getters.nextCourseOf(getters.currentCourse.subject);
   },
   offdays: (state, getters) => {
-    let offdays
-    let offdaySeries = getters.setting('schedule/offdays') // get offdays
-                              .split('\n') // split by line
-                              .forEach(offday => offday.replace('\r', '')) // fuck windows
-    for (const offdaySerie of offdaySeries) { // for each line
-      if (offdaySerie.match(/.+\s*-\s*.+/)) { // if its a range (start date - end date)
+    let offdays;
+    let offdaySeries = getters
+      .setting("offdays") // get offdays
+      .split("\n") // split by line
+      .forEach(offday => offday.replace("\r", "")); // fuck windows
+    for (const offdaySerie of offdaySeries) {
+      // for each line
+      if (offdaySerie.match(/.+\s*-\s*.+/)) {
+        // if its a range (start date - end date)
         // get start & end: split by "-", trim spaces, parse as date
-        let [start, end] = offdaySerie.split('-').reduce(e => pdate(e.trim()))
+        let [start, end] = offdaySerie.split("-").reduce(e => pdate(e.trim()));
         // iterate over each date
-        while(start.isBefore(end)) {
+        while (start.isBefore(end)) {
           // add in the array
-          offdays.push(start)
-          start = start.add(1, 'day')
+          offdays.push(start);
+          start = start.add(1, "day");
         }
       } else {
         // add in the array
-        offdays.push(pdate(offdaySerie))
+        offdays.push(pdate(offdaySerie));
       }
     }
-    return offdays
+    return offdays;
   }
 };
 
@@ -239,10 +256,10 @@ export const mutations = {
     state.courses = courses;
   },
   SET_ADDITIONS(state, additions) {
-    state.additions = additions
+    state.additions = additions;
   },
   SET_DELETIONS(state, deletions) {
-    state.deletions = deletions
+    state.deletions = deletions;
   }
   /*  UPDATE_EVENT(state, courseId, newCourse) {
     let course = state.getters
@@ -252,5 +269,4 @@ export const mutations = {
   }, */
 };
 
-export const actions = {
-};
+export const actions = {};
