10b83edafacac11ede16ad7079e144baf82f7caa
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-19 15:52:02 +0200

    add subjects to store

--- store/index.js
+++ store/index.js
@@ -1,15 +1,19 @@
 export const state = () => ({
+    subjects: []
 })
 
 export const getters = { 
-
+    subjects: (state, getters) => {
+        return state.subjects
+    }
 }
 
 export const mutations = {
-
+    SET_SUBJECTS: (state, subjects) => {
+        state.subjects = subjects
+    }
 }
 
 export const actions = {
-    test() {
-    }
- }
+
+}
