310049d2930a0260875aa5237b69b083cab7d8ba
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-19 15:49:05 +0200

    TextBlockInput

---
+++ components/TextBlockInput.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+textarea.TextBlockInput
+</template>
+
+<script>
+
+export default {
+    name: 'TextBlockInput',
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/defaults'
+
+.TextBlockInput
+    border-radius: 10px
+    width: 100%
+    height: 200px
+    background: var(--grey)
+    margin-top: 10px
+    padding: 10px
+</style>
