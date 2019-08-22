52dd5e54e81c3155abed45b673eefe40eb1763e9
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-22 09:37:28 +0200

    fix layout issues + formatting

--- pages/notes/_uuid.vue
+++ pages/notes/_uuid.vue
@@ -8,6 +8,7 @@
     MainGroupLeft
     MainGroupRight
   //TODO: synchronized scrolling
+  //TODO: button to upload source file to textarea
 
 .container
   BarFloating
@@ -49,74 +50,76 @@
 
 <script>
 //--- essentials ---
-import axios from 'axios'
-import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+import axios from "axios";
+import moment from "moment";
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 //--- components ---
-import TheHeading from '~/components/TheHeading.vue'
-import ArrayButtonFlat from '~/components/ArrayButtonFlat.vue'
-import ButtonFlat from '~/components/ButtonFlat.vue'
-import MainGroup from '~/components/MainGroup.vue'
-import MainGroupLeft from '~/components/MainGroupLeft.vue'
-import MainGroupRight from '~/components/MainGroupRight.vue'
-import ButtonIcon from '~/components/ButtonIcon.vue'
-import BarFloating from '~/components/BarFloating.vue'
-import moment from 'moment';
+import TheHeading from "~/components/TheHeading.vue";
+import ArrayButtonFlat from "~/components/ArrayButtonFlat.vue";
+import ButtonFlat from "~/components/ButtonFlat.vue";
+import MainGroup from "~/components/MainGroup.vue";
+import MainGroupLeft from "~/components/MainGroupLeft.vue";
+import MainGroupRight from "~/components/MainGroupRight.vue";
+import ButtonIcon from "~/components/ButtonIcon.vue";
+import BarFloating from "~/components/BarFloating.vue";
 
-export default {  
-    components: {
-        TheHeading,
-        MainGroup,
-        MainGroupLeft,
-        MainGroupRight,
-        BarFloating,
-        ButtonIcon
-    },
+export default {
+  layout: "bare",
+  components: {
+    TheHeading,
+    MainGroup,
+    MainGroupLeft,
+    MainGroupRight,
+    BarFloating,
+    ButtonIcon
+  },
 
-    async asyncData({ store, app, route }) {
-      let error = false
+  async asyncData({ store, app, route }) {
+    let error = false;
+    try {
+      const { data } = await app.$axios.get(`/notes/`);
+      store.commit("notes/SET_NOTES", data);
+    } catch (e) {
+      console.error(e);
+    }
+
+    if (!store.getters.allSettings.length) {
       try {
-        const { data } = await app.$axios.get(`/notes/`);
-        store.commit("notes/SET_NOTES", data);
+        const { data } = await app.$axios.get("/settings/");
+        store.commit("SET_SETTINGS", data);
       } catch (e) {
         console.error(e);
       }
+    }
 
-      if (!store.getters.allSettings.length) {
-        try {
-          const { data } = await app.$axios.get('/settings/')
-          store.commit('SET_SETTINGS', data)
-        } catch (e) {
-          console.error(e)
-        }
-      }
-
-      try {
-        let { uuid } = route.params
-        const { data } = await app.$axios.get(`/notes/${uuid}/`)
-        return {
-          name: data.name,
-          subject: data.subject,
-          created: data.created,
-          server_last_modified: data.last_modified,
-          server_content: data.content,
-          uuid: data.uuid
-        }
-      } catch (e) {
-        if (e.response.status == 404) {
-          error = "Note non trouvée."
-          return {
-            error
-          }
-          // app.$router.push('/notes')
-        }
-      }
-    },
-
-    data() {
+    try {
+      let { uuid } = route.params;
+      const { data } = await app.$axios.get(`/notes/${uuid}/`);
       return {
-        content: 'Chargement...'
+        name: data.name,
+        subject: data.subject,
+        created: data.created,
+        server_last_modified: data.last_modified,
+        server_content: data.content,
+        uuid: data.uuid
+      };
+    } catch (e) {
+      if (e.response.status == 404) {
+        error = "Note non trouvée.";
+        return {
+          error
+        };
+        // app.$router.push('/notes')
+      }
+    }
+  },
+
+  data() {
+    return {
+      content: "Chargement...",
       lastSave: moment()
-    },
+    }
+  },
 
   mounted() {
     let content;
@@ -146,23 +149,33 @@ export default {
         event.preventDefault()
         this.sync()
       }
-    },
+    })
 
-    watch: {
-      content(newContent, oldContent) {
-        window.localStorage.setItem(`${this.uuid}--noteContent`, newContent)
-        window.localStorage.setItem(`${this.uuid}--noteLastModified`, moment().format())
-        this.content = newContent
-      }
-    },
+    // Rough scroll syncing
+    document.getElementById("editor").addEventListener("scroll", event => {
+      document.getElementById("mirror").scrollTop = event.target.scrollTop;
+    });
 
-    computed: {
-      ...mapGetters({
-        note: 'notes/note'
-      })
-    },
+  },
 
-    methods: {
+  watch: {
+    content(newContent, oldContent) {
+      window.localStorage.setItem(`${this.uuid}--noteContent`, newContent);
+      window.localStorage.setItem(
+        `${this.uuid}--noteLastModified`,
+        moment().format()
+      );
+      this.content = newContent;
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      note: "notes/note"
+    })
+  },
+
+  methods: {
     async uploadToServer(last_modified, content, force = false) {
       if (moment().diff(this.lastSave, "seconds") < 5 && !force) {
         this.$toast.error(
@@ -172,34 +185,42 @@ export default {
       } else {
         this.lastSave = moment();
       }
-          const { data } = await this.$axios.patch(`/notes/${this.uuid}/`, {last_modified, content})
-          this.$toast.success('Synchronisation réussie')
-        } catch (e) {
-          errored = true
-          this.$toast.error(`Erreur lors de la synchronisation: ${e}`)
-        }
-        if (!errored) {
-          window.localStorage.removeItem(`${this.uuid}--noteContent`)
-          window.localStorage.removeItem(`${this.uuid}--noteLastModified`)
-        }
-      },
-      sync() {
-        this.uploadToServer(moment().format(), this.content)
-      },
-      save_source() {
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.content));
-        element.setAttribute('download', this.name);
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
+      let errored = false;
+      try {
+        const { data } = await this.$axios.patch(`/notes/${this.uuid}/`, {
+          last_modified,
+          content
+        });
+        this.$toast.success("Synchronisation réussie");
+      } catch (e) {
+        errored = true;
+        this.$toast.error(`Erreur lors de la synchronisation: ${e}`);
       }
+      if (!errored) {
+        window.localStorage.removeItem(`${this.uuid}--noteContent`);
+        window.localStorage.removeItem(`${this.uuid}--noteLastModified`);
+      }
+    },
+    sync() {
+      this.uploadToServer(moment().format(), this.content);
+    },
+    save_source() {
+      var element = document.createElement("a");
+      element.setAttribute(
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(this.content)
+      );
+      element.setAttribute("download", this.name);
+
+      element.style.display = "none";
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
     }
-}
+  }
+};
 </script>
 
 <style lang="sass" scoped>
@@ -209,14 +230,20 @@ export default {
   height: 100%
   width: 100%
   font-family: 'Roboto Mono', monospace
+  font-weight: 300
   font-size: 18px
-.MainGroup [class^=MainGroup]
-  height: 100vh
-.mirror
+.MainGroupLeft, .MainGroupRight
+  margin-top: 0
+  height: calc(100vh - 200px)
+  width: 100%
+  max-width: 50vw
 #mirror
+  height: 100%
+  width: 100%
   overflow-y: scroll
+  overflow-x: hidden
   line-height: 1.3
-  font-size: 24px
+  font-size: 18px
   *
     word-break: everywhere
 </style>
