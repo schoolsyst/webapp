86ca91ac2086c12d40b1c4f10540362f99612a1c
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-23 14:32:09 +0200

    accomodate size change of CardNoteFile + put a "Nouvelle note..." title

--- components/CardNoteAdd.vue
+++ components/CardNoteAdd.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-    li.CardNoteAdd
+    li.CardNoteAdd(open-modal="add-note" open-at="center")
         BaseCard.card
-            button(@click="$emit('click')")
+            button
                 i.material-icons add
+        p.title Nouvelle note...
 </template>
 
 <script>
@@ -22,15 +23,27 @@ export default {
 
 <style lang="sass" scoped>
 @import '~/assets/defaults'
-.CardNoteFile 
-    height: 300px
-    width: 200px
+.CardNoteAdd
+    height: 320px
+    width: 220px
+    &:hover
+        cursor: pointer
 
 .card 
     background: var(--offset-blue)
-    height: 250px
+    height: 270px
     width: 100%
     margin-bottom: 10px
+    display: flex
+    align-items: center
+    justify-content: center
+
+.title 
+    color: var(--blue)
+    text-align: center
+    font-weight: 500
+    font-size: 18px
+    line-height: 1.2em
     display: flex
     align-items: center
     justify-content: center
@@ -38,4 +51,8 @@ export default {
 button i
     color: var(--blue)
     font-size: 100px
+button:focus
+    outline: none
+    i
+        
 </style>
