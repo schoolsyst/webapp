91bce6d3f75dd336a7ccb4be22442549858090f1
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-08-25 04:40:12 +0200

    add HTML layer for CardTest (needs JS & CSS)

--- components/CardTest.vue
+++ components/CardTest.vue
@@ -1,20 +1,70 @@
 <template lang="pug">
-div.CardTest
-    code {{note}}
+div.CardTest(
+    :style="{backgroundColor: subject.color}"
+)
+    .top-bar
+        .text
+            span.subject-abbr {{subject.abbreviation}}
+            span.percentage {{totalProgressDisp}}%
+        .current-progress(:data-progress="totalProgress")
+    ul.notes
+        li(v-for="note in notes" :key="note.uuid")
+            CardTestNoteItem(v-if="expanded", v-bind="note")
+            template(v-else) {{ note.name }}
+    .infos(v-if="expanded")
+        p.grade-and-room
+            |sur 
+            span.info {{grade[0].maximum}}] 
+            |coef 
+            span.info {{grade[0].weight}}] 
+            |en 
+            span.info {{room}}]
+        p.date
+            |le
+            span.info {{moment(date, 'YYYY-MM-DD').format('DD MMMM YYYY')}}
+    button.expand
+        i.material-icons
+            template(v-if="expanded") chevron_up
+            template(v-else) chevron_down
+
+
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import CardTestNoteItem from '~/components/CardTestNoteItem.vue'
 
 export default {
     name: 'CardTest',
+
+    components: {CardTestNoteItem},
 
     props: {
         subject: Object,
         name: String,
         due: String,
         note: Object
-    }
+    },
+
+    computed: {
+        totalProgress() {
+            if (!notes.length) return -1
+
+            let totalProgress = 0
+            this.notes.forEach(note => {
+                if(note.progress !== -1) {
+                    totalProgress += note.progress
+                }
+            });
+
+            return totalProgress / this.notes.length
+        },
+
+        totalProgressDisp() {
+            if (this.totalProgress === -1) return '?'
+            return this.totalProgress * 100
+        }
+    },
 }
 </script>
 
---
+++ components/CardTestNoteItem.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+div.CardTestNoteItem
+    .top
+        span.name {{name}}
+        span.progress-percentage {{progressDisp}}%
+    .progress-bar(
+        :data-progress="progress"
+        :class="{'undefined': progress === -1}"
+    )
+        .progress-bar-total
+    nuxt-link.learn(
+        :to="`/learn/${uuid}/`",
+        v-if="progress < 1"
+    )
+        i.material-icons(v-if="progress === -1") add
+        i.material-icons(v-else) arrow_right
+        span.btn-label Réviser
+    p.learn.finished(v-else)
+        i.material-icons check
+        span.btn-label Révision terminée
+</template>
+
+<script>
+
+export default {
+    name: 'CardTestNoteItem',
+    props: {
+        name: String,
+        progress: Number,
+        uuid: String
+    },
+    computed: {
+        progressDisp() {
+            if (this.progress === -1) {
+                return '?'
+            } else {
+                return this.progress * 100
+            }
+        },
+    },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/defaults'
+
+</style>
