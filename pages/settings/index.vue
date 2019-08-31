f15266fc732994c39470a97b7557e7653b735822
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-08-31 11:15:11 +0200

    basic settings page: HTML+CSS done

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,39 +1,32 @@
-<template>
-<!-- COMPONENT TREE
-Excluding single-use components (TheHeading, TheNavbar, TheFooter,...)
+<template lang="pug">
+//- COMPONENT TREE
+    Excluding single-use components (TheHeading, TheNavbar, TheFooter,...)
 
-ArrayButtonFlat
-MainGroup
-    MainGroupLeft
-    MainGroupRight
--->
+    ArrayButtonFlat
+    MainGroup
+        MainGroupLeft
+        MainGroupRight
 
-<div class="container">
-    <TheHeading>Réglages</TheHeading>
-    <ArrayButtonFlat>
-        <ButtonFlat @click="$router.push('/logout')" icon="power_off">
-            <nuxt-link to="/logout">Déconnexion</nuxt-link>
-        </ButtonFlat>
-    </ArrayButtonFlat>
-    <MainGroup>
-        <MainGroupLeft>
-            <HeadingSub>Réglages</HeadingSub>
-            <dl>
-                <div v-for="(setting, i) in settings" :key="i">
-                    <dt>{{name}} [<code>{{key}}</code>]</dt>
-                    <dd>{{value}}</dd>
-                </div>
-            </dl>
-        </MainGroupLeft>
-        <MainGroupRight>
-            <HeadingSub></HeadingSub>
-        </MainGroupRight>
-    </MainGroup>
-</div>
+.container
+    TheHeading Réglages
+    ArrayButtonFlat
+        ButtonFlat(@click="$router.push('/logout')" icon="power_off")
+            nuxt-link(to="/logout") Déconnexion
+    MainGroup
+        MainGroupLeft
+            template(v-for="(settings, namespace) in groupedSettings")
+                template(v-if="!namespace.startsWith('__')")
+                    HeadingSub {{namespace}}
+                    .field(v-for="setting in settings")
+                        label(:for="`field_${setting.key.replace('_', '-')}`") {{setting.name}}
+                        textarea(:id="`field_${setting.key.replace('_', '-')}`") {{getSetting(setting.key)}}
+
 </template>
 
 <script>
 import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+import groupBy from 'lodash.groupby'
+//-----------------------------------------------------------------
 import TheHeading from '~/components/TheHeading.vue'
 import ArrayButtonFlat from '~/components/ArrayButtonFlat.vue'
 import ButtonFlat from '~/components/ButtonFlat.vue'
@@ -52,23 +45,37 @@ export default {
         MainGroupRight,
         HeadingSub,
     },
-
-    async asyncData() {
-        let settings
-        try {
-            const { data } = await this.$axios.get('/settings/')
-            settings = data
-        } catch(error) {
-            settings = null
-        }
-        return {
-            settings
-        }
+    computed: {
+        ...mapGetters({
+            defaultSettings: 'defaultSettings',
+            setting: 'setting'
+        }),
+        groupedSettings() {
+            return groupBy(this.defaultSettings, 'namespace')
+        },
     },
+    methods: {
+        getSetting(key) {
+            try {
+                return this.setting(key).value
+            } catch(error) {
+                console.warn(`Cannot find setting ${key}`)
+                return null
+            }
+        }
+    }
 }
 </script>
 
-<style lang="sass" scoped>
-@import '~/assets/defaults'
-
+<style lang="stylus" scoped>
+.field
+    display flex
+    align-items center
+    margin-bottom 20px
+    label
+        width 300px
+    textarea
+        padding 10px
+        background var(--grey)
+        border-radius 7.5px
 </style>
