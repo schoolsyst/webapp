82d637aee403501b34e0547fcf5d17aa7a955a70
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-11-24 23:19:06 +0100

    register page

---
+++ pages/register.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+  .container
+    OverlayLoadingLogo(animation="animate-in-compound")
+    .login-link
+      p
+        | Déjà un compte ?
+        ButtonNormal(small variant="outline" href="/login") Connectez-vous
+    form(method="post" @submit.prevent="register")
+      .group-left
+        InputField(
+          name="username"
+          v-model="username"
+          v-bind="{validation}"
+          tabindex="1"
+        ) Nom d'utilisateur
+        InputField(
+          type="password"
+          v-model="password"
+          v-bind="{validation}"
+          tabindex="3"
+        ) Mot de passe
+      .group-right
+        InputField(
+          name="email"
+          v-model="email"
+          v-bind="{validation}"
+          tabindex="2"
+        ) Adresse email
+        InputField(
+          type="password"
+          name="password-confirmation"
+          v-model="passwordConfirmation"
+          v-bind="{validation}"
+          tabindex="4"
+        ) Confirmer le mot de passe
+      .group-both
+        ButtonNormal(variant="primary" v-bind="{validation}") Créez votre compte
+</template>
+
+<script>
+import OverlayLoadingLogo from '~/components/OverlayLoadingLogo.vue'
+import InputField from '~/components/InputField.vue'
+import ButtonNormal from '~/components/ButtonNormal.vue'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  layout: 'bare',
+  auth: 'guest', // Disable auth requirements
+  components: { OverlayLoadingLogo, InputField, ButtonNormal },
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    }
+  },
+  computed: {
+    validation() {
+      return this.validateRegister()(this)
+    }
+  },
+  methods: {
+    ...mapGetters('auth', ['validateRegister']),
+    ...mapActions('auth', ['register'])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container
+  height 100vh
+  width 100vw
+  display flex
+  justify-content center
+  align-items center
+  flex-direction column
+  text-align: center
+@media (min-width: 1001px)
+  form
+    display grid
+    grid-template-columns repeat(2, 1fr)
+    grid-gap 2em
+  .group-both
+    grid-column 1 / span 2
+    width 100%
+    display flex
+    justify-content center
+
+.login-link
+  margin-bottom: 4em
+.forgotten-password
+  margin-top: 2em
+.submit
+  margin-top: 1em
+
+.field
+  width: 300px
+</style>
