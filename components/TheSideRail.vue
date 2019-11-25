ab5ae0ed9c4fbb8fb736216490fe0f2f0ef6c83c
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-11-25 17:57:34 +0100

    slightly bigger icons, support for notifications

--- components/TheSideRail.vue
+++ components/TheSideRail.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
   nav#side-rail
     ul
-      li.item(v-for="(link, i) in sideRailLinks" :key="link.href" :class="{current: isCurrent(link)}")
-        nuxt-link.link(:to="link.href")
+      li.item(v-for="(link, i) in sideRailLinks" :key="link.href")
+        nuxt-link.link(
+          :to="link.href"
+          :class="{current: isCurrent(link), notifications: hasNotifications(link)}"
+        )
           Icon.icon(:filled="isCurrent(link)") {{link.icon}}
           span.name {{link.name}}
+          span.notifications-badge(v-if="hasNotifications(link)")
 </template>
 
 <script>
@@ -18,6 +22,11 @@ export default {
     isCurrent(link) {
       const topPathFragment = this.$route.path.split('/')[1]
       return '/' + topPathFragment === link.href
+    },
+    hasNotifications(link) {
+      if (link.hasOwnProperty('notifications')) {
+        return link.notifications >= 1 || link.notifications === '99+'
+      }
     }
   }
 }
@@ -32,16 +41,31 @@ nav
   justify-content center
   width 4em
   overflow hidden
-  margin-bottom: 2em
+  margin-bottom: 1.5em
   padding .25em 0
 .link
   text-align center
-  color var(--blue)
+  color var(--black)
+  &:hover, &.current
+    color var(--blue)
+  &:hover .name
+      opacity: 1
+  position relative
+.link .notifications-badge
+  position absolute
+  display block
+  background var(--red)
+  height 0.80em
+  width 0.80em
+  border-radius 50%
+  top: 0
+  right: 25%
 .icon
-  font-size 1.75em
+  font-size 1.85em
   display block
 .name
   white-space nowrap
   text-overflow ellipsis
-  font-size: 0.75em
+  font-size: 0.85em
+  opacity: 0
 </style>
