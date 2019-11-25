11a87cbdadc9f1e55f288c15aa15f7c659544ab5
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-11-25 23:26:37 +0100

    continue <TheDrawer>

--- components/TheDrawer.vue
+++ components/TheDrawer.vue
@@ -1,8 +1,17 @@
 <template lang="pug">
-    nav#drawer(:expanded="expanded")
+    nav#drawer
         ul
-            li(v-for="link in links" :key="href")
-                nuxt-link(:to="link.href" :class="{current: isCurrent(link.href)}").link
+            li.user 
+                span.username {{ $auth.user.username }}
+                br
+                span.email {{ $auth.user.email }}
+            li(
+                v-for="link in links"
+                :key="href"
+                :class="{current: isCurrent(link.href)}"
+            )
+                hr(v-if="link === 'separator'")
+                nuxt-link.link(v-else :to="link.href")
                     Icon.icon {{link.icon}}
                     span.name {{link.name}}
 
@@ -13,12 +22,6 @@ import Icon from '~/components/Icon.vue'
 
 export default {
     components: { Icon },
-    props: {
-        expanded: {
-            type: Boolean,
-            default: false
-        }
-    },
     data() {
         return {
             links: [
@@ -51,14 +54,24 @@ export default {
                     name: 'Sac',
                     href: '/bag', 
                     icon: 'work_outline'
+                },
+                'separator',
+                {
+                    name: 'Réglages',
+                    href: '/settings',
+                    icon: 'settings'
+                },
+                {
+                    name: 'Signaler un bug',
+                    href: '/bug-report',
+                    icon: 'bug_report'
                 }
-
             ]
         }
     },
     methods: {
         isCurrent(href) {
-            const topPathFragment = this.$route.path.split('/')[0]
+            const topPathFragment = this.$route.path.split('/')[1]
             return '/' + topPathFragment === href
         }
     }
@@ -66,7 +79,43 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+nav
+    opacity: 0.95
+    position absolute
+    left 0
+    bottom 0
+    top 0
+    padding-top: 30px
+    display flex
+    background var(--white)
+    z-index: 1000
+    width: 300px
+    overflow hidden
+    transition width .25s ease
+    mix-blend-mode darken
+    box-shadow 0 8px 10px -5px rgba(0,0,0,0.2),0 16px 24px 2px rgba(0,0,0,0.14),0 6px 30px 5px rgba(0,0,0,0.12)
+.username
+    font-size: 24px
+.email
+    font-size: 14px
 .link
     display flex
     align-items center
+    .icon
+        font-size: 32px
+    .name
+        margin-left: 20px
+        font-size: 18px
+        white-space nowrap
+
+li
+    padding 18px 24px
+    &.current
+        color var(--blue)
+        background var(--offset-blue)
+        border-top-right-radius 60px
+        border-bottom-right-radius 60px
+hr
+    margin 5px 0
+    opacity: 0.25
 </style>
