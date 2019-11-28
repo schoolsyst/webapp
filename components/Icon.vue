4c8460922f87e834f886f73ba7a859d88cf9cc0b
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-11-28 18:20:38 +0100

    add [filled] prop

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,3 +1,30 @@
 <template lang="pug">
-    i.material-icons-outlined: slot
+    i(:class="className") {{iconText}}
 </template>
+
+<script>
+export default {
+    props: {
+        filled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        className() {
+            return 'material-icons' + (!this.filled ? '-outlined' : '')
+        },
+        iconText() {
+            const rawText = this.$slots.default[0].text
+            console.log(this.$slots.default)
+            if (this.filled) {
+                return rawText.replace('_outline','')
+            } else {
+                return rawText
+            }
+        }
+    }
+}
+
+
+</script>
