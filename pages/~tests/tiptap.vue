dc1b49b8acebf2b47677595b2574552785a6323c
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-12-07 23:28:17 +0100

    filled tiptap editor with a demo of all to-be-supported html features
    need to setup a solid stylesheet before starting anything

--- pages/~tests/tiptap.vue
+++ pages/~tests/tiptap.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
     .container
+        PickerSubject(name="tiptap")
         .toolbar
             .top
                 nuxt-link(to="/notes"): Icon.icon arrow_back
-                h1(contenteditable) Title
+                h1
+                    BadgeSubject.subject(name="Русский", color="#F57C00" clickable open-modal="tiptap-subject-picker")
+                    span(contenteditable) Title
 
             EditorMenuBar(:editor="editor" v-slot="{commands, isActive}")
                 .menubar
@@ -106,7 +109,10 @@
                     button(@click="commands.code_block" title="Bloc de code")
                         Icon code
                     button(title="Bloc de maths (Ctrl + Shift + E)"): Icon functions
-        .editor-page-wrapper: editor-content.editor-page(:editor="editor")
+                    button(title="Image"): Icon image
+        .editor-page-wrapper
+            .editor-page(v-html="content")
+            //- editor-content.editor-page(:editor="editor")
 </template>
 
 <script>
@@ -137,9 +143,11 @@ import {
     BulletList,
 } from 'tiptap-extensions'
 import Icon from '~/components/Icon.vue'
+import BadgeSubject from '~/components/BadgeSubject.vue'
+import PickerSubject from '~/components/PickerSubject.vue'
 
 export default {
-    components: { Editor, EditorContent, EditorMenuBar, Icon },
+    components: { Editor, EditorContent, EditorMenuBar, Icon, BadgeSubject, PickerSubject },
     layout: 'bare',
     data() {
         return {
@@ -168,8 +176,55 @@ export default {
                     new History(),
                     new BulletList(),
                 ],
-                content: '<p>Just a good ol\' p-tag</p>'
+                content: ``
             }),
+            content: `
+                    <h1>First header</h1>
+                    <h2>Second header</h2>
+                    <h3>Third header</h3>
+                    <h4>Fourth header</h4>
+                    <h5>Fifth header</h5>
+                    <h6>Smallest header</h6>
+                    <p> 
+                       <br> Footnotes<sup><a href="#fn-1-def" id="fn-1-usage-1">1</a></sup>
+                       <br> <u>Underlined</u>
+                       <br> <s>Strikethrough</s> 
+                       <br> <em>Italic</em> 
+                       <br> <strong>Bold</strong> 
+                       <br> <code>Monospace</code> 
+                       <br>  <sup>Super</sup>script 
+                       <br>  <sub>Sub</sub>script 
+                       <br>  <a href="#">Named link</a>  
+                       <br>  <mark> Highlighted </mark> 
+                       <br>  <math> <mi>mαth</mi> </math> 
+                       <br>  <ol> <li> Ordered </li> <li> Lists </li> </ol> 
+                       <br>  <ul> <li> Unordered </li> <li> Lists </li> </ul> 
+                       <br>  <abbr title="Support For Abbreviations Is Useful"> SFAIU </abbr> 
+                       <br>  <dl> <dt> Definition </dt> <dd> Lists </dd> <dd> And </dd> <dt> Multiple </dt> <dd> Terms </dd> </dl> 
+                       <br> <span style="color: red"> colored </span> <span style="color: cyan"> text </span>
+                    </p>
+                    <code><pre>
+                    A code block.
+                    Lorem ipsum dolor sit amet.
+                    </pre></code>
+                    <blockquote>A blockquote </blockquote>
+                    An horizontal rule:
+                    <hr>
+
+                    <center> <table> <tr> <td> A </td><td> table </td> </tr> <tr> <td> with </td> <td> rows. </td> </tr> </table> </center>
+
+                    <center> <math title="E=m c^2 e^(i pi)=-1 AA x in ZZ (sin^2x+cos^2x=1) sum_(i=1)^n i^3=((n(n+1))/2)^2 "><mstyle displaystyle="true" mathsize="1em"><mi>E</mi><mo>=</mo><mi>m</mi><msup><mi>c</mi><mn>2</mn></msup><msup><mi>e</mi><mrow><mi>i</mi><mi>π</mi></mrow></msup><mo>=</mo><mo>-</mo><mn>1</mn><mo>∀</mo><mi>x</mi><mo>∈</mo><mo>ℤ</mo><mrow><mo>(</mo><msup><mo>sin</mo><mn>2</mn></msup><mi>x</mi><mo>+</mo><msup><mo>cos</mo><mn>2</mn></msup><mi>x</mi><mo>=</mo><mn>1</mn><mo>)</mo></mrow><mrow><munderover><mo>∑</mo><mrow><mi>i</mi><mo>=</mo><mn>1</mn></mrow><mi>n</mi></munderover></mrow><msup><mi>i</mi><mn>3</mn></msup><mo>=</mo><msup><mrow><mo>(</mo><mfrac><mrow><mi>n</mi><mrow><mo>(</mo><mi>n</mi><mo>+</mo><mn>1</mn><mo>)</mo></mrow></mrow><mn>2</mn></mfrac><mo>)</mo></mrow><mn>2</mn></msup></mstyle></math></center>
+
+                    <center> <img src="https://picsum.photos/800/500/"> </center>
+
+                    <p class="admonition admonition-note"> Admonition block, normal </p>
+                    <p class="admonition admonition-danger"> Admonition block, 'danger' version </p>
+
+                    <p> And finally, some normal text!</p>
+
+                    <hr id="fn-sep">
+                    <ol> <li id="fn-1-def"> The footnote's content. <a href="#fn-1-usage-1"> ↑ </a></li> </ol>
+                `
         }
     },
     beforeDestroy() {
@@ -198,7 +253,11 @@ body
     display flex
     align-items center
     h1
-        margin-left .25em
+        display flex
+        align-items center
+        margin-left 1.5em
+        .subject
+            margin-right .75em
     .icon
         font-size: 2em
 .menubar
@@ -216,6 +275,8 @@ body
     &.active
         color var(--blue)
         background var(--offset-blue)
+    &:hover
+        color var(--blue)
 .low-opacity, .sep
     opacity: 0.25
 .sep
@@ -248,4 +309,14 @@ body
     line-height 1.2em
     p
         margin-bottom 0.2em
+
+    .admonition
+        &::before
+            content 'more_vert'
+            font-family 'Material Icons'
+            
+        &-danger
+            background var(--offset-red)
+        &-note
+            background var(--offset-blue)
 </style>
