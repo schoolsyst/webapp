3bb849e308f58c30cb65fead25fd675d8b942050
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-12-16 08:44:06 +0100

    allow scrolling in <TheDrawer>

--- components/TheDrawer.vue
+++ components/TheDrawer.vue
@@ -70,7 +70,7 @@ nav
     background var(--white)
     z-index: 1000
     width: 20em
-    overflow hidden
+    overflow-y auto
     transition left .375s ease
     box-shadow 0 8px 10px -5px rgba(0,0,0,0.2),0 16px 24px 2px rgba(0,0,0,0.14),0 6px 30px 5px rgba(0,0,0,0.12)
 .image
