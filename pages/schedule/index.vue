0f93545a36ba337795ff3af6784f0fef4c657fd6
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-12-16 09:00:05 +0100

    started /schedule/

--- pages/schedule/index.vue
+++ pages/schedule/index.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
-  .container
+  .container.-sideby-side
+    .mutations-area
+      HeadingSub Cours supprimés / reportés
+      ul.mutations
+        li.new: ButtonNormal Nouveau cours reporté ou supprimé
+        li(v-for="mutation in mutations" :key="mutation.uuid")
+          | {{ mutation }}
     .schedule
+      HeadingSub Emploi du temps
       Schedule(:now="scheduleDate")
 </template>
 
 <script>
 import Schedule from '~/components/Schedule.vue'
+import HeadingSub from '~/components/HeadingSub.vue'
+import ButtonNormal from '~/components/ButtonNormal.vue'
 import { mapGetters, mapState } from 'vuex'
 export default {
-  components: { Schedule },
+  components: { Schedule, HeadingSub, ButtonNormal },
   head: {
     title: 'Emploi du temps'
   },
   computed: {
     ...mapState(['now']),
+    ...mapGetters('schedule', ['mutations']),
     scheduleDate() {
       return this.now
     }
@@ -23,3 +33,13 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.schedule
+  overflow-x auto
+  // Cheap hack, if overflow-x is auto, overflow-y becomes either auto or scroll.
+  // See: https://stackoverflow.com/a/39554003
+  padding-bottom 500px 
+h2
+  margin-bottom 1em
+</style>
