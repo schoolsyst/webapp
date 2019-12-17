98aeb89bbb8c830deee2c4104399579593b868eb
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-12-17 23:05:38 +0100

    back to <BaseModal> custom implementation, vue-js-modal broken af
    was just awfully broken on mobile, when on the site it says 'mobile-ready'... my implementation was better anyway, but only missing the way more pratical $modal.show/$modal.hide calls instead of putting [open-modal] on elements.
    
    Gotta change the plugin file name and the method names (to $modal.open and $modal.close)

---
+++ components/BaseModal.bak.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  vue-js-modal(
+    :name="name"
+    height="auto"
+    :class="{'no-header': !title && !closeButton}"
+  )
+    slot
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    closeButton: {
+      type: Boolean,
+      default: true
+    },
+    title: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+modal-padding = 2rem
+
+// /deep/ .v--modal-box
+//   background: var(--white)
+//   color var(--black)
+//   padding: (modal-padding)
+//   padding-top (modal-padding + 2rem)
+//   overflow: auto !important
+//   box-shadow none
+//   @media (min-width 651px)
+//     border-radius: var(--border-radius)
+//     max-width: calc(100vw - 2rem)
+//     max-height: calc(100vh - 2rem)
+//   @media (max-width 650px)
+//     width 100vw !important
+//     // left: 0 !important
+//     // right: 0 !important
+//     // top: 0 !important
+//     min-height 100vh !important
+
+// .no-header /deep/ .v--modal-box
+//   padding-top (modal-padding)
+
+button.close-modal
+  position absolute
+  top 10px
+  right 10px
+  i
+    font-size 2em
+    color var(--black)
+.title
+  position absolute
+  top: 15px
+  left: 15px
+  font-size 1.4em
+</style>
--- components/BaseModal.vue
+++ components/BaseModal.vue
@@ -1,63 +1,100 @@
-<template lang="pug">
-  vue-js-modal(:name="name" height="auto" :class="{'no-header': !title && !closeButton}")
-    p.title(v-if="title") {{ title }}
-    button.close-modal(v-if="closeButton", @click="$modal.hide(name)")
-      i.material-icons close
-    .modal-content: slot
-</template>
+<template lang="pug"> 
+//https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/dialog.html 
+aside.BaseModal(:id="`modal_${name}`", 
+    aria-hidden="true" 
+    aria-modal="false" 
+    role="dialog" 
+    @click.self="$modal.hide(name)" 
+    :class="{'edge-to-edge': edgeToEdge}"
+) 
+    .modal-wrapper
+        .header(v-if="closeButton || title")
+          p.title(v-if="title") {{ title }}
+          button.close-modal(
+            v-if="closeButton",
+            @click="$modal.hide(name)"
+          ): Icon close
+        slot 
+ 
+</template> 
+ 
+<script> 
+import Icon from '~/components/Icon.vue'
 
-<script>
-export default {
-  props: {
-    name: String,
-    closeButton: {
+export default { 
+  name: "BaseModal", 
+  components: { Icon },
+  props: { 
+    name: String, 
+    closeButton: { 
+      type: Boolean, 
+      default: true, 
+    }, 
+    edgeToEdge: {
       type: Boolean,
-      default: true
+      default: false
     },
     title: String
+  }, 
+  data() { 
+    return { wasFocused: null } 
   }
-}
-</script>
+} 
+</script> 
+ 
+<style lang="stylus" scoped> 
+.BaseModal 
+    position: fixed 
+    top: 0 
+    left: 0 
+    width: 100% 
+    height: 100% 
+    display: flex 
+    &.centered 
+        justify-content: center 
+        align-items: center 
+    &.opened 
+        background: var(--modal-backdrop)
+        opacity: 1 
+    &:not(.opened) 
+        pointer-events: none 
+        opacity: 0 
+        background: transparent 
+    z-index: 10000 
+    transition: all .25s ease 
+ 
+.modal-wrapper 
+    position: relative 
+    border-radius: var(--border-radius) 
+    background: var(--white) 
+    color var(--black) 
+    padding: 20px
+    overflow: auto
+    max-height 100vh
+    max-width 100vw
+    @media (max-width 650px)
+      width 100vw
 
-<style lang="stylus" scoped>
-modal-padding = 2rem
-
-/deep/ .v--modal-box
-  background: var(--white)
-  color var(--black)
-  padding: (modal-padding)
-  padding-top (modal-padding + 2rem)
-  overflow: auto !important
-  box-shadow none
-  @media (min-width 651px)
-    border-radius: var(--border-radius)
-    max-width: calc(100vw - 2rem)
-    max-height: calc(100vh - 2rem)
-  @media (max-width 650px)
-    width 100vw !important
-    // min-height 100vh !important
-    left: 0 !important
-    right: 0 !important
-    top: 0 !important
-    height auto !important
-    // bottom: 0 !important
-
-.no-header /deep/ .v--modal-box
-  padding-top (modal-padding)
-
-button.close-modal
-  position absolute
-  top 10px
-  right 10px
+.edge-to-edge 
+  .modal-wrapper
+    padding: 0
+  .header
+    padding: 20px
+ 
+.BaseModal:not(.centered) .modal-wrapper 
+    position: fixed
+ 
+.header
+  display: flex
+  padding 0.5em 0.5em 1.5em 0.5em
+  align-items center
+button.close-modal 
+  margin-left auto
   i
     font-size 2em
     color var(--black)
 .title
-  position absolute
-  top: 15px
-  left: 15px
   font-size 1.4em
-
-/deep/ .modal-content
-  overflow-y scroll
-</style>
+  margin-right 2vw
+  
+</style> 
