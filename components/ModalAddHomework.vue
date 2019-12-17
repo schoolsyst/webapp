b736e636d366c504397b8b28103b19db143de652
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-12-17 23:11:55 +0100

    <ModalAddHomework> (WIP)

--- components/ModalAddHomework.vue
+++ components/ModalAddHomework.vue
@@ -1,18 +1,35 @@
 <template lang="pug">
-.multimodal
-  PickerSubject(parent-modal="add-homework" @pick="pickSubject")
-  BaseModal(name="add-homework" show-close-button)
-    .title-area
-      p Ajouter des devoirs
-    .name-area
-      InputFlat(name="name" placeholder="Titre du devoir…" big)
-    .notes-area
-      TextBlockInput(name="notes" placeholder="Notes additionnelles…")
-    .due-area
-      HeadingSub À finir pour…
-      InputFlat(name="due" placeholder="JJ/MM/AAAA")
-    .type-area
-      RadioButtons(:values="homeworkTypes") Type de devoir
+BaseModal(name="add-homework" title="Ajouter des devoirs")
+  PickerSubject(parent-modal="add-homework" @pick="subject = $event")
+  BadgeSubject.subject.badge(
+    clickable
+    @click="$modal.show('add-homework-subject-picker')"
+    v-bind="subject"
+  ).mobile-only
+  SubjectDot.subject.dot(
+    clickable
+    @click="$modal.show('add-homework-subject-picker')"
+    v-bind="subject"
+  ).desktop-only
+  InputField.name(
+    v-model="name"
+    placeholder="Nom de la note..."
+    no-error-messages
+    no-label
+    variant="filled"
+  )
+  .notes-area
+    InputField(
+      name="notes" 
+      type="block"  
+    ) Notes additionnelles
+  .due-area
+    InputField(
+      name="due" 
+      type="date"
+    ) À finir pour...
+  .type-area
+    RadioButtons(:values="homeworkTypes") Type de devoir
 </template>
 
 <script>
@@ -20,9 +37,10 @@ import BaseModal from '~/components/BaseModal.vue'
 import PickerSubject from '~/components/PickerSubject.vue'
 import HeadingSub from '~/components/HeadingSub.vue'
 import RadioButtons from '~/components/RadioButtons.vue'
+import InputField from '~/components/InputField.vue'
 
 export default {
-  components: { BaseModal, PickerSubject, HeadingSub, RadioButtons },
+  components: { BaseModal, PickerSubject, HeadingSub, RadioButtons, InputField },
   data() {
     return {
       homeworkTypes: [
@@ -30,7 +48,11 @@ export default {
         { key: "TEST",     label: "Contrôle"      },
         { key: "DM",       label: "Devoir maison" },
         { key: "TOBRING",  label: "À apporter"    },
-      ]
+      ],
+      subject: null,
+      details: null,
+      due: null,
+      name: null
     }
   }
 }
--- pages/homework/index.vue
+++ pages/homework/index.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
     .container
+        ModalAddHomework
         ul.homework(v-if="!!all.length")
             li(v-for="hw in all", :key="hw.uuid")
                 CardHomework(v-bind="hw")
-        ScreenEmpty(v-else)
+        ScreenEmpty(v-else @cta="$modal.show('add-homework')")
             template(#smiley) \o/
             p Bravo. Vous n'avez plus rien à travailler, pour le moment.
             template(#cta) Ajouter des devoirs
@@ -12,9 +13,10 @@
 <script>
 import CardHomework from '~/components/CardHomework.vue'
 import ScreenEmpty from '~/components/ScreenEmpty.vue'
+import ModalAddHomework from '~/components/ModalAddHomework.vue'
 import { mapGetters } from 'vuex';
 export default {
-    components: { CardHomework, ScreenEmpty },
+    components: { CardHomework, ScreenEmpty, ModalAddHomework },
     head: {
         title: 'Devoirs'
     },
