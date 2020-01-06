0bc935c7ed9483a843de7b0755d7e345318b3c55
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2020-01-06 23:46:56 +0100

    🚧 /settings/: Continue

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
+    //TODO: move ul.categories > li to <InputSetting>, to reuse in setup
+    //TODO: center-align, less width, index=portal to one page per category + subjects page + schedule page
     .container
         .-side-by-side
             ul.categories
                 li(v-for="g in grouped")
                     HeadingSub {{ g[0] }}
                     ul.settings
-                        li(v-for="s in g[1]" :key="s.uuid").-side-by-side
-                            .description
-                                | {{ s.description }}
-                            .input
+                        li(v-for="s in g[1]" :key="s.uuid")
+                            .input(v-tooltip="s.description || false")
                                 template(v-if="s.type === 'SELECT'" )
                                     RadioButtons(
                                         v-if="s.choices.length < 5"
                                         :values="s.choices"
                                         :value="s.value"
-                                        @select="setValue({key: s.key, value: $event})"
+                                        @input="setValue({key: s.key, value: $event})"
                                         :name="s.key"
                                     ) {{ s.name }}
                                     multiselect(
@@ -25,12 +25,13 @@
                                         :show-labels="false"
                                         :name="s.key"
                                     )
+                                //TODO: proper multiple field
                                 InputField(
                                     v-else
-                                    :value="value()(s.uuid)",
+                                    :value="s.value",
                                     @input="setValue({key: s.key, value: $event })",
                                     :name="s.key"
-                                    :type="s.type"
+                                    :type="s.multiple ? 'block' : s.type.toLowerCase()"
                                 ) {{ s.name }}
             ul.subjects
                 li(v-for="subject in subjects")
@@ -51,15 +52,33 @@ export default {
     components: { HeadingSub, InputField, CardSubject, Multiselect, Checkbox, RadioButtons },
     methods: {
         ...mapActions('settings', ['post', 'setValue']),
-        ...mapGetters('settings', ['value'])
+        ...mapGetters('settings', ['value']),
     },
     computed: {
         ...mapGetters('settings', ['grouped', 'all']),
         ...mapGetters('subjects', { subjects: 'all' })
     },
     async mounted() {
-        await this.$store.dispatch('settings/load')
-        await this.$store.dispatch('subjects/load')
+        this.$withLoadingScreen(async () => {
+            await this.$store.dispatch('settings/load')
+            await this.$store.dispatch('subjects/load')
+        })
     }
 }
 </script>
+
+<style lang="stylus" scoped>
+ul.categories li
+    display flex
+    flex-direction column
+    justify-content center
+    margin-bottom 1em
+    p.description
+        margin-bottom: 0.5em
+        margin-top: 0.5em
+        font-size: 0.9em
+        text-align center
+        font-style italic
+h2
+    margin-bottom: 0.5em
+</style>
