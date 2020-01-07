3734ee19f2e7cc740410f055461e7388659b9dad
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-01-07 19:53:08 +0100

    🚧 /settings/: Revamp subjects cards

--- components/CardSubject.vue
+++ components/CardSubject.vue
@@ -1,343 +1,77 @@
 <template lang="pug">
-BaseCard.CardSubject(:style="{backgroundColor: mutColor, color: textColor}", :class="{expanded}")
-  //TODO: type in the name of the subject to confirm deletion
-  ModalDialogConfirm.confirm-delete(
-    :name="`delete-subject-${uuid}`" 
-    confirm-role="danger" 
-    confirm-text="Supprimer" 
-    challenge-message="Pour confirmer, écrivez le nom de la matière"
-    :challenge="(input) => input === name"
-    @confirm="deleteSubject(); $emit('expand', false)"
-  )
-    | Supprimer cette matière supprimera #[strong tout] ce qui était en lien avec:
-    br
-    | &nbsp; &bull; devoirs, #[br]
-    | &nbsp; &bull; contrôles, #[br]
-    | &nbsp; &bull; évenements dans l'emploi du temps #[br]
-    | &nbsp; &bull; prises de notes #[br]
-    br
-    | Cette action est #[strong irréversible]
-  p.name 
-    input.name-input(v-model="mutName" placeholder="Nom de la matière..." :readonly="!expanded")
-    //-TODO: close button reverts values to those passed in props
-    ButtonIcon.edit-button(@click="toggleAndCancel(); $emit('expand', true)" :color="textColor" v-if="uuid !== 'new' && !noEditButton") {{ expanded ? 'close' : 'edit' }}
-  .row
-    .field
-      LabelFlat Abbreviation*
-      input.abbreviation(maxlength="3" v-model="mutAbbreviation" placeholder="ABC")
-    .field
-      LabelFlat Couleur*
-      PickerColor.color(
-        v-model="mutColor"
-        :color="textColor"
-        :modal-id="uuid"
-      )
-    .field
-      LabelFlat Objectif
-      BigNumber.grade-goal(
-        small
-        :writables="['value']"
-        @input="mutGrade_goal = $event",
-        :value="grade_goal * gradeUnit"
-        :unit="`/${gradeUnit}`"
-      )
-  .row
-    .field
-      LabelFlat Salle par défaut
-      input.room(v-model="mutRoom")
-    .field
-      LabelFlat Poids total
-      BigNumber.physical-weight(
-        small
-        :writables="['value']"
-        @input="mutPhysical_weight = $event",
-        :value="physical_weight"
-        unit="kg"
-      )
-  .button-row
-    ButtonIcon.delete-button(:open-modal="`confirm-delete-subject-${uuid}`" open-at="center" :color="textColor" v-if="uuid !== 'new'") delete
-    ButtonIcon.confirm-button(@click="$emit('expand', false); uploadChanges()" :color="textColor") check
+  .card-wrapper(
+      :style="{backgroundColor, borderColor}"
+    )
+    BadgeSubject.color-dot(
+      v-if="!fullColor"
+      no-tooltip variant="dot"
+      v-bind="{color, name}"
+    )
+    span.name(:style="{color: textColor}") {{ name }}
+    //- button.action(:style="{color: textColor}")
+    //-   Icon: slot(name="action-icon") edit
 </template>
 
 <script>
-import BigNumber from "~/components/BigNumber.vue"
-import ButtonIcon from "~/components/ButtonIcon.vue"
-import LabelFlat from "~/components/LabelFlat.vue"
-import PickerColor from "~/components/PickerColor.vue"
-import ModalDialogConfirm from "~/components/ModalDialogConfirm.vue"
-import BaseCard from "~/components/BaseCard.vue"
-import InputFlat from "~/components/InputFlat.vue"
-import { mapGetters } from "vuex"
-import tinycolor from "tinycolor2"
-import debounce from "lodash.debounce"
-import slugify from "slugify"
+import Icon from '~/components/Icon.vue'
+import BadgeSubject from '~/components/BadgeSubject.vue'
+import tinycolor from 'tinycolor2'
+import { mapGetters } from 'vuex'
 
 export default {
-  name: "CardSubject",
-
-  components: {
-    BigNumber,
-    ButtonIcon,
-    LabelFlat,
-    InputFlat,
-    PickerColor,
-    ModalDialogConfirm,
-    BaseCard,
-  },
-
+  components: {Icon, BadgeSubject},
   props: {
-    color: {
-      type: String,
-      default: "#000000",
-    },
     name: String,
-    uuid: String,
-    grade_goal: Number,
-    room: String,
-    physical_weight: Number,
-    abbreviation: String,
-    slug: String,
-    noEditButton: {
-      type: Boolean,
-      default: false,
+    color: String,
+    weight: {
+      type: Number,
+      default: 1,
+      validate: (val) => val >= 0
     },
-    expanded: {
-      type: Boolean,
-      default: false,
+    goal: {
+      type: Number,
+      validate: (val) => val >= 0 && val <= 1
     },
-  },
-
-  data() {
-    return {
-      mutColor: this.color,
-      mutName: this.name,
-      mutGrade_goal: this.grade_goal,
-      mutRoom: this.room,
-      mutPhysical_weight: this.physical_weight || 0,
-      mutAbbreviation: this.abbreviation,
-      isEditing: this.editing,
+    // Options
+    fullColor: {
+      type: Boolean,
+      default: false
     }
   },
-
   computed: {
-    ...mapGetters({
-      setting: "settings/value",
-    }),
-    gradeUnit() {
-      return Number(this.setting("grade_max"))
+    backgroundColor() {
+      return this.fullColor ? this.color : null
+    },
+    borderColor() {
+      if (this.fullColor) return this.color
     },
     textColor() {
-      return tinycolor(this.mutColor).isLight() ? "black" : "white"
-    },
-    subject() {
-      return {
-        color: this.mutColor,
-        name: this.mutName,
-        grade_goal: isNaN(this.mutGrade_goal)
-          ? null
-          : Number(this.mutGrade_goal) / this.gradeUnit,
-        room: this.mutRoom,
-        physical_weight: this.mutPhysical_weight,
-        abbreviation: this.mutAbbreviation.toLowerCase(),
-        slug:
-          this.slug ||
-          slugify(`${this.$auth.user.username}--${this.mutName}`).toLowerCase(),
-      }
-    },
+      if (this.fullColor) return this._textColor()(this.color)
+    }
   },
-
   methods: {
-    toggleAndCancel() {
-      if (this.expanded) {
-        this.mutColor = this.$props.color
-        this.mutName = this.$props.name
-        this.mutGrade_goal = this.$props.grade_goal
-        this.mutRoom = this.$props.room
-        this.mutPhysical_weight = this.$props.physical_weight
-        this.mutAbbreviation = this.$props.abbreviation
-        this.mutSlug = this.$props.slug
-      }
-      this.$emit('expand', !this.expanded)
-    },
-    uploadChanges: debounce(function() {
-      try {
-        let res
-        if (this.uuid === "new") {
-          res = this.$axios.post(`/subjects/`, this.subject)
-          this.$store.commit("subjects/ADD", res.data)
-          this.$toast.success(`Matière "${res.data.name}" ajoutée`)
-        } else {
-          res = this.$axios.patch(`/subjects/${this.uuid}/`, this.subject)
-          this.$store.commit("subjects/PATCH", {
-            uuid: this.uuid,
-            data: res.data,
-          })
-          this.$toast.success(`Matière "${res.data.name}" modifiée`)
-        }
-      } catch (error) {
-        let verb
-        if (this.uuid === "new") {
-          verb = "création"
-        } else {
-          verb = "modification"
-        }
-        this.$toast.error(`Erreur lors de la ${verb} de la matière: ${error}`)
-      }
-    }, 500),
-    deleteSubject() {
-      try {
-        this.$axios.delete(`subjects/${this.uuid}/`)
-        this.$store.commit("DELETE_SUBJECT", this.uuid)
-      } catch (error) {
-        this.$toast.error(
-          `Erreur lors de la suppression de la matière: ${error}`
-        )
-      }
-    },
-  },
+    ...mapGetters({ _textColor: 'textColor'})
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-.CardSubject {
-  width: 600px;
-  max-width: calc(100% - 40px);
-  padding: 10px 20px !important;
-  height: 300px;
-
-  @media (max-width: 888px) {
-    height: 400px;
-  }
-
-  overflow: hidden;
-
-  &:not(.expanded) {
-    height: 60px;
-  }
-
-  transition: all 0.25s ease;
-}
-
-.confirm-delete /deep/ .message {
-  text-align: left;
-}
-
-// Fix some colors
-.BigNumber /deep/ *, input {
-  color: inherit;
-}
-
-.BigNumber {
-  margin-top: -5px;
-  line-height: 36px !important;
-
-  & /deep/ .value {
-    height: 36px;
-    font-size: 36px;
-  }
-
-  & /deep/ .unit {
-    font-size: calc(2 / 3 * 36px);
-    line-height: calc(2 / 3 * 36px);
-    opacity: 1;
-  }
-}
-
-input:not(.name-input) {
-  font-size: 36px;
-  padding-bottom: 5px; // Correct 'g'/'q's being cut off
-  width: 200px;
-}
-
-// TODO: make a new <LabelFlat> (call it <LabelAlt>)
-.LabelFlat {
-  text-transform: none !important;
-  opacity: 1 !important;
-  font-weight: normal !important;
-  font-size: 22px !important;
-}
-
-.row {
-  display: grid;
-  grid-template-columns: repeat(3, 200px);
-
-  @media (max-width: 888px) {
-    grid-template-columns: repeat(2, 50%);
-  }
-
-  grid-gap: 10px;
-  margin-top: 25px;
-}
-
-.button-row {
-  position: relative;
-}
-
-.confirm-button, .delete-button {
-  position: absolute;
-}
-
-.confirm-button {
-  @media (min-width: calc(888px + 1px)) {
-    right: 0px;
-    top: -20px;
-  }
-
-  @media (max-width: 888px) {
-    right: 0;
-    top: calc(-40px + 40px);
-  }
-
-  & /deep/ .icon {
-    font-size: 48px;
-  }
-}
-
-.delete-button {
-  @media (min-width: calc(888px + 1px)) {
-    right: 65px;
-    top: -10px;
-  }
-
-  @media (max-width: 888px) {
-    left: 0px;
-    top: calc(-30px + 40px);
-  }
-
-  & /deep/ .icon {
-    font-size: 32px;
-  }
-}
-
-.edit-button /deep/ .icon {
-  font-size: 36px;
-}
-
-.name {
-  height 75px {
-    @media (max-width: 888px) {
-      height: 60px;
-    }
-  }
-
-  input, p {
-    font-size: 28px;
-
-    @media (max-width: 888px) {
-      font-size: 20px;
-    }
-  }
-
-  margin-bottom: 20px;
-  font-weight: bold;
-  display: grid;
-  align-items: center;
-  grid-template-columns: calc(100% - 36px) 36px;
-}
-
-.abbreviation {
-  text-transform: uppercase;
-  font-family: 'Roboto Mono', monospace;
-  letter-spacing: 2px;
-}
+.card-wrapper
+  display flex
+  align-items center
+  max-width 450px
+  padding 1em 1.2em
+  border solid 2px var(--grey-light)
+  border-radius var(--border-radius)
+  cursor pointer
+  .color-dot
+    font-size: 1.3em
+    margin-right: 0.5em
+  .action
+    margin-left auto
+  .action /deep/ i
+    color inherit
+    font-size: 1.5em
+  .name
+    font-size: 1.1em
 </style>
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -2,33 +2,46 @@
     //TODO: move ul.categories > li to <InputSetting>, to reuse in setup
     //TODO: center-align, less width, index=portal to one page per category + subjects page + schedule page
     .container
+        ModalAddSubject(v-model="newSubject")
         .-side-by-side
             ul.categories
                 li(v-for="g in grouped")
                     HeadingSub {{ g[0] }}
                     ul.settings
                         li(v-for="setting in g[1]" :key="setting.uuid")
-                            InputSetting(v-bind="{...setting, _key: setting.key}")
-            ul.subjects
-                li(v-for="subject in subjects")
-                    CardSubject(v-bind="subject")
+                            InputSetting.input(v-bind="{...setting, _key: setting.key}")
+            .subjects-wrapper
+                HeadingSub Matières
+                ul.subjects
+                    li.new(@click="$modal.show('add-subject')")
+                        Icon add
+                    li(v-for="subject in subjects")
+                        CardSubject(v-bind="subject")
 
 </template>
 
 <script>
 import HeadingSub from '~/components/HeadingSub.vue'
-import InputField from '~/components/InputField.vue'
 import CardSubject from '~/components/CardSubject.vue'
-import Multiselect from 'vue-multiselect'
-import 'vue-multiselect/dist/vue-multiselect.min.css'
-import Checkbox from '~/components/Checkbox.vue'
 import InputSetting from '~/components/InputSetting.vue'
+import Icon from '~/components/Icon.vue'
+import InputField from '~/components/InputField.vue'
+import ModalAddSubject from '~/components/ModalAddSubject.vue'
 import { mapActions, mapGetters } from 'vuex'
 export default {
-    components: { HeadingSub, InputField, CardSubject, Multiselect, Checkbox, InputSetting },
+    components: { HeadingSub, InputField, CardSubject, InputSetting, Icon, ModalAddSubject },
+    data() {
+        return {
+            newSubject: {
+                name: "",
+                color: "#000000",
+                weight: 1
+            }
+        }
+    },
     computed: {
         ...mapGetters('settings', ['grouped', 'all']),
-        ...mapGetters('subjects', { subjects: 'all' })
+        ...mapGetters('subjects', { subjects: 'all', sortSubjects: 'orderBy' })
     },
     async mounted() {
         this.$withLoadingScreen(async () => {
@@ -44,7 +57,7 @@ ul.categories li
     display flex
     flex-direction column
     justify-content center
-    margin-bottom 1em
+    margin-bottom 1.5rem
     p.description
         margin-bottom: 0.5em
         margin-top: 0.5em
@@ -53,4 +66,23 @@ ul.categories li
         font-style italic
 h2
     margin-bottom: 0.5em
+
+ul.subjects li
+    margin-bottom 1em
+    &.new
+        background var(--blue-offset)
+        color var(--blue)
+        border-radius var(--border-radius)
+        display flex
+        justify-content center
+        align-items center
+        width 100%
+        max-width 450px
+        height: 100px
+        cursor pointer
+        & /deep/ i
+            font-size: 3rem
+        &:hover
+            background var(--blue-offset-dark)
+            color var(--blue-dark)
 </style>
