1980f4ce7a123666bc67d597e719695a0267c2ec
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2020-01-13 21:34:07 +0100

    ✨ <PickerColor> Remake

--- components/PickerColor.vue
+++ components/PickerColor.vue
@@ -1,37 +1,58 @@
 <template lang="pug">
-.PickerColor
-    BaseModal.picker(:name="modalName" :close-button="false")
-        ul.swatches
-            li.swatch(
-                v-for="preset in presets" :key="preset.hex"
-                :style="{backgroundColor: preset.hex}"
-                :title="preset.name"
-                @click="$emit('input', preset.hex); $modal.hide(modalName)"
+  //TODO: Label
+  //TODO: inherit from <BaseInput>
+  .color-picker
+    .swatch.opener(
+      :style="{backgroundColor: value}"
+      @click="$modal.open(modalName)"
+      :title="colorName"
+    )
+    BaseModal(:name="modalName" title="Choisir une couleur...")
+      form
+        .swatches(name="swatches")
+          .swatch(
+            v-for="preset in sortedPresets" :key="preset.hex"
+            :style="{backgroundColor: '#' + preset.hex}"
+            @click="$emit('input', '#' + preset.hex); $modal.close(modalName)"
+            :title="preset.name"
+          )
+        .custom-color
+          .swatch(
+            :style="{backgroundColor: value}"
+            :title="colorName"
+          )
+          label
+            Icon.native-picker-opener(
+              v-tooltip="'Couleur personnalisée'"
+            ) colorize
+            input(
+              type="color"
+              :value="value.toUpperCase()" 
+              @input="$emit('input', $event.target.value)"
+              :name="`${modalName}--custom`"
             )
-        //- hr.sep
-        .hex-input
-            span.octothorpe #
-            input.hex(
-              type="text"
-              maxlength="6"
-              :value="value.substring(1)"
-              @input="emitInputEvent($event)"
-            )
-    .color-picker(@click="$modal.show(modalName)" :class="value")
+          .octothorpe #
+          InputField.hex-input(
+            :value="value.toUpperCase().replace('#', '')"
+            @input="$emit('input', '#' + $event)"
+            no-error-messages no-label no-action-button
+            :name="`${modalName}--hex-input`"
+          )
 </template>
 
 <script>
 import tinycolor from "tinycolor2"
+import { firstBy } from 'thenby'
 import BaseModal from "~/components/BaseModal.vue"
+import Icon from '~/components/Icon.vue'
 import InputField from '~/components/InputField.vue'
-export default {
-  name: "PickerColor",
 
+export default {
   components: {
     BaseModal,
     InputField,
+    Icon
   },
-
   props: {
     value: {
       type: String,
@@ -39,130 +60,124 @@ export default {
     },
     namespace: String,
   },
-
   data() {
     return {
       presets: [
         //From: https://material.io/resources/color/
-        { hex: "#c62828", name: "Rouge" },
-        { hex: "#ff5722", name: "Orange profond" },
-        { hex: "#ef6c00", name: "Orange" },
-        { hex: "#fdd835", name: "Jaune" },
-        { hex: "#8bc34a", name: "Vert clair" },
-        { hex: "#2e7d32", name: "Vert" },
-        { hex: "#26a69a", name: "Turquoise" },
-        { hex: "#00e5ff", name: "Cyan" },
-        { hex: "#29b6f6", name: "Bleu clair" },
-        { hex: "#1565c0", name: "Bleu" },
-        { hex: "#003c8f", name: "Bleu foncé" },
-        { hex: "#3f51b5", name: "Indigo" },
-        { hex: "#5e35b1", name: "Violet profond" },
-        { hex: "#9c27b0", name: "Violet" },
-        { hex: "#e91e63", name: "Rose profond" },
-        { hex: "#f06292", name: "Rose" },
-        { hex: "#ffc1e3", name: "Rose clair" },
-        { hex: "#a7c0cd", name: "Bleu-gris" },
-        { hex: "#bdbdbd", name: "Gris" },
-        { hex: "#6d4c41", name: "Marron" },
+        { hex: "c62828", name: "Rouge" },
+        { hex: "ff5722", name: "Orange profond" },
+        { hex: "ef6c00", name: "Orange" },
+        { hex: "fdd835", name: "Jaune" },
+        { hex: "8bc34a", name: "Vert clair" },
+        { hex: "2e7d32", name: "Vert" },
+        { hex: "26a69a", name: "Turquoise" },
+        { hex: "00e5ff", name: "Cyan" },
+        { hex: "29b6f6", name: "Bleu clair" },
+        { hex: "1565c0", name: "Bleu" },
+        { hex: "003c8f", name: "Bleu foncé" },
+        { hex: "3f51b5", name: "Indigo" },
+        { hex: "5e35b1", name: "Violet profond" },
+        { hex: "9c27b0", name: "Violet" },
+        { hex: "e91e63", name: "Rose profond" },
+        { hex: "f06292", name: "Rose" },
+        { hex: "ffc1e3", name: "Rose clair" },
+        { hex: "a7c0cd", name: "Bleu-gris" },
+        { hex: "bdbdbd", name: "Gris" },
+        { hex: "6d4c41", name: "Marron" },
+        { hex: "B38C80", name: "Marron clair" },
       ],
     }
   },
-
   computed: {
     modalName() {
+      if (!this.namespace) return 'color-picker'
       return `${this.namespace}-color-picker`
-    }
-  },
-
-  methods: {
-    emitInputEvent($event) {
-      let val = $event.target.value
-      if ([3, 6].includes(val.length)) {
-        this.$emit("input", "#" + val)
-      }
     },
+    colorName() {
+      if (!this.value || typeof this.value !== 'string') return false
+      let value = this.value.toUpperCase()
+      let preset = this.presets.find(p => '#' + p.hex.toUpperCase() === value)
+      return preset ? `${preset.name} (${value})` : value
+    },
+    sortedPresets() {
+      return this.presets.sort(
+        firstBy(col => tinycolor(col).toHsl().h)
+      )
+    }
   },
 }
 </script>
 
 <style lang="stylus" scoped>
-.color-picker {
-  height: 48px;
-  width: 48px;
+//FIXME: Bad, copy-pasted from <RadioButtons>
+.BaseModal /deep/ 
+  .content
+    display flex
+    justify-content center
 
-  &.black {
-    border: 3px solid rgba(0, 0, 0, 0.75);
-  }
+legend
+  padding 0 10px
+  margin-bottom: 5px
+  text-transform uppercase
+  letter-spacing 1px
+  font-size 0.75em
+  font-weight 500
+  display block
+.opener
+  cursor pointer
+  border-radius var(--border-radius)
 
-  &.white {
-    border: 3px solid rgba(255, 255, 255, 0.75);
-  }
-
-  cursor: pointer;
-}
-
-.picker /deep/ .modal-wrapper {
-  padding: 0;
-  overflow: hidden; // Fix weird scrollbar
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.swatches {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-}
-
-.swatch {
-  height: 36px;
-  width: 36px;
-
-  @media (max-width: 888px) {
-    height: 52px;
-    width: 52px;
-  }
-
-  cursor: pointer;
-}
-
-.sep {
-  width: 50px;
-  color: black;
-  margin: 10px 0;
-}
-
-.hex {
-  width: 70px;
-
-  @media (max-width: 888px) {
-    width: 100px;
-  }
-
-  text-transform: uppercase;
-}
-
-.octothorpe {
-  opacity: 0.5;
-  margin: 0 5px;
-
-  @media (max-width: 888px) {
-    margin-left: 15px;
-  }
-
-  color: black;
-}
-
-.hex-input {
-  font-family: var(--fonts-monospace)
-  display: flex;
-  align-items: center;
-  padding: 10px 0; // padding-bottom on modal-wrapper doesn't work
-
-  @media (max-width: 888px) {
-    font-size: 24px;
-    padding: 20px 0;
-  }
-}
+swatch-width = 2.5em
+.swatch
+  display block
+  height swatch-width
+  width swatch-width
+.swatches 
+  // justify-content center
+  margin-bottom 1em
+  display flex
+  flex-wrap wrap
+  width: swatch-width * 7
+  @media (max-width 375px)
+    width swatch-width * 6
+  input
+    display none
+  .swatch
+    border-radius 0
+    height swatch-width
+    width swatch-width
+    cursor pointer
+.custom-color
+  hex-input-padding = 0.5em
+  font-family var(--fonts-monospace-light)
+  display flex
+  align-items center
+  // justify-content center
+  width 100%
+  height swatch-width
+  .swatch
+    width "calc(%s / 2)" % swatch-width
+    margin-right: 0.25em
+  label
+    display flex
+    align-items center
+  .native-picker-opener
+    font-size: 1.5em
+    cursor pointer
+  input
+    display none
+  .octothorpe
+    color var(--grey-dark)
+    margin-left auto
+    margin-right 0.5em
+    font-size: 1.2em
+  .hex-input
+    height swatch-width
+  .hex-input
+  .hex-input /deep/ input
+    width "calc(6ch + %s * 2 + 5px)" % hex-input-padding
+    font-family var(--fonts-monospace-light) !important //FIXME: make a 'font-family' opt on <InputField>
+  .hex-input /deep/ input
+    min-width "calc(6ch + %s * 2 + 5px)" % hex-input-padding
+    padding hex-input-padding
 </style>
