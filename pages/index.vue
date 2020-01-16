fd8138e12388622ec35014f936c8b0781264edde
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2020-01-16 18:56:20 +0100

    🚸 /: Display loading screen, re-fetch user

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,15 @@
 
 <script>
 export default {
+  layout: 'bare',
   mounted() {
-    let step = this.$auth.user.setup_step
-    console.log(step)
-    if (step) this.$router.push(`/setup/${step}`)
-    else this.$router.push('/timeline')
+    this.$withLoadingScreen(async () => {
+      await this.$auth.fetchUser()
+      let step = this.$auth.user.setup_step
+      console.log(step)
+      if (step) this.$router.push(`/setup/${step}`)
+      else this.$router.push('/timeline')
+    }, { title: 'Veuillez patienter...', subtitle: 'On vérifie que tout soit bien dans l\'ordre'})
   }
 }
 </script>
