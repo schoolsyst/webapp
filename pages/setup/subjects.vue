12f696952eedf68a3712a28777c261ce9596c1d2
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-01-18 16:40:31 +0100

    🚑 setup/subjects: Fix "next" link, add help text

--- pages/setup/subjects.vue
+++ pages/setup/subjects.vue
@@ -2,13 +2,14 @@
   .container
     ModalAddSubject(v-model="newSubject" @post="post({subject: $event})")
     h1 Ajoutez vos matières.
+    p.help Ajoutez toutes les matières qui sont dans votre emploi du temps,#[br]même celles dans lesquelles vous n'aurez pas de notes (ex. Culture religieuse)
     button.add(@click="$modal.show('add-subject')"): Icon add
     ul.subjects
       li(v-for="subject in all" :key="subject.uuid")
         CardSubject(@click="$modal.show('edit-subject')" v-bind="subject")
     TheBottomBar
       ButtonNormal(variant="text-blue" href="/logout") #[Icon cancel] Annuler
-      ButtonNormal.to-right(variant="primary" href="/setup/settings") Continuer
+      ButtonNormal.to-right(variant="primary" href="/setup/schedule/settings") Continuer
 </template>
 
 <script>
@@ -51,24 +52,24 @@ export default {
   justify-content center
   align-items center
   flex-direction column
+  padding-bottom: 100px
+p.help
+  text-align center
+h1
+  margin-top: 25vh
 .add /deep/ i
   font-size 5em
   padding 1em
-ul.subjects 
+ul.subjects
   display flex
-  flex-wrap wrap
-  flex-shrink 0
-  flex-grow 0
-  max-width 1000px
-  @media (max-width 1000px)
-    max-width 100vw
+  flex-direction column
   justify-content center
   align-items center
-  spacing = 1.5em
+  width 1000px
+  @media (max-width 1000px)
+    max-width 100vw
   li
-    width: "calc(450px + %s)" % spacing
-    margin-bottom spacing
-  li:last-child
-    flex-grow 1
-    margin-left spacing
+    margin 0 auto
+    width 450px //ref: <CardSubject> max-with
+    margin-bottom 1.5em
 </style>
