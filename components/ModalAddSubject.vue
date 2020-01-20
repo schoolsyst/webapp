a282afaa7c0dbcf7be5717eb7ab9fc39b9619393
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-01-20 21:50:03 +0100

    ✨ Finish <ModalAddSubject>

--- components/ModalAddSubject.vue
+++ components/ModalAddSubject.vue
@@ -3,24 +3,39 @@
     :name="modalName"
     title="Ajouter une matière..."
   )
-    form.content(@submit.prevent="$emit('post', value); resetInputs(); $modal.close(modalName)")
+    form.content(@submit.prevent="$emit('post', subjectObject); resetInputs(); $modal.close(modalName)")
       .color-and-name
-        PickerColor(v-model="value.color" :namespace="modalName") Couleur
+        PickerColor(
+          v-model="color"
+          :namespace="modalName"
+          tooltip="Choisir la couleur"
+        ) Couleur
         InputField(
-          v-model="value.name"
+          v-model="name"
           variant="filled"
           name="name"
           v-bind="{validation}"
         ) Nom
-      InputField(
-        v-model="value.weight"
-        variant="filled"
-        name="weight"
-        type="number"
-        v-bind="{validation}"
-        tabindex="3"
-        no-error-messages
-      ) Coefficient
+      .grades-related
+        InputField(
+          v-model="weight"
+          variant="filled"
+          name="weight"
+          type="number"
+          v-bind="{validation}"
+          tabindex="3"
+          no-error-messages
+        ) Coefficient
+        InputField(
+          v-model="goal"
+          variant="filled"
+          name="goal"
+          type="number"
+          v-bind="{validation}"
+          tabindex="4"
+          :placeholder="`sur ${gradesUnit}`"
+          no-error-messages
+        ) Objectif de moyenne
       .submit: ButtonNormal(
         variant="primary"
         v-bind="{validation}"
@@ -43,7 +58,7 @@ export default {
       default: () => ({
         color: '#000000',
         name: null,
-        weight: null
+        weight: 1
       })
     },
     modalName: {
@@ -51,17 +66,53 @@ export default {
       default: 'add-subject'
     }
   },
-  computed: {
-    validation() {
-      if (Object.keys(this.value).length === 0)
-        return { validated: true, errors: {} }
-      return this.validate()(this.value)
+  data() {
+    return {
+      name: null,
+      color: '#000000',
+      weight: 1,
+      goal: null
     }
+  },
+  computed: {
+    subjectObject() {
+      return {
+        name: this.name,
+        weight: this.weight,
+        color: this.color,
+        goal: this.goal / this.gradesUnit
+      }
+    },
+    validation() {
+      return this.validate(this.$store.getters)(this.subjectObject)
+    },
+    gradesUnit() {
+      return this.settingValue()('grade_max')
+    }
+  },
+  watch: {
+    value() {
+      if (this.value) {
+        const { name, weight, color } = this.value
+        this.name = name
+        this.weight = weight
+        this.color = color
+      }
+    }
+  },
+  mounted() {
+    this.$withLoadingScreen(async () => {
+      await this.$store.dispatch('settings/load')
+    })
   },
   methods: {
     ...mapGetters('subjects', ['validate']),
+    ...mapGetters('settings', { settingValue: 'value' }),
     resetInputs() {
-      this.$emit('input', {})
+      this.name = ''
+      this.color = '#000000'
+      this.weight = 1
+      this.goal = null
     }
   }
 }
@@ -78,6 +129,16 @@ export default {
     & /deep/ .opener
       margin-top: -0.5em
       margin-right: 1em
+
+.grades-related
+  display flex
+  align-items center
+  .field:first-child /deep/ input
+    max-width: 110px
+  .field:nth-child(2) /deep/ input
+    max-width: 200px
+  .field:first-child
+    margin-right 1rem
 
 .submit
   margin-top: 1.5em
