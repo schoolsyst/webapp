fcf54de0573a587cb0b7c41153d9220608c4a033
ewen-lbh <ewen.lebihan7@gmail.com>
2020-01-24 10:00:03 +0100

    💄 /setup/subjects: side-by-side layout, css fixes

--- pages/setup/subjects.vue
+++ pages/setup/subjects.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
   .container
+    // TODO: #beta-1.1.0 modal anchored in page, no need to click a btn
     ModalAddSubject(v-model="newSubject" @post="post({subject: $event})")
-    h1 Ajoutez vos matières.
-    p.help Ajoutez toutes les matières qui sont dans votre emploi du temps,#[br]même celles dans lesquelles vous n'aurez pas de notes (ex. Culture religieuse)
-    button.add(@click="$modal.show('add-subject')"): Icon add
-    ul.subjects
-      li(v-for="subject in all" :key="subject.uuid")
-        CardSubject(@click="$modal.show('edit-subject')" v-bind="subject")
+    .-side-by-side
+      .left
+        h1 Ajoutez vos matières.
+        p.help Ajoutez toutes les matières qui sont dans votre emploi du temps,#[br]même celles dans lesquelles vous n'aurez pas de notes (ex. Culture religieuse)
+        button.add(@click="$modal.show('add-subject')"): Icon add
+      .right
+        ul.subjects
+          li(v-for="subject in all" :key="subject.uuid")
+            CardSubject(@click="$modal.show('edit-subject')" v-bind="subject")
     TheBottomBar
       ButtonNormal(variant="text-blue" href="/logout") #[Icon cancel] Annuler
       ButtonNormal.to-right(variant="primary" href="/setup/schedule/settings") Continuer
@@ -60,11 +64,15 @@ export default {
   flex-direction: column
   padding-bottom: 100px
 
-p.help
-  text-align: center
+@media (max-width 650px)
+  .left
+    text-align center
+  h1
+    margin-top: 25vh
 
-h1
-  margin-top: 25vh
+p.help, h1
+  margin 0 10px
+
 
 .add /deep/ i
   font-size: 5em
@@ -75,13 +83,14 @@ ul.subjects
   flex-direction: column
   justify-content: center
   align-items: center
-  width: 1000px
+  width: 100%
 
   @media (max-width: 1000px)
-    max-width: 100vw
+    max-width: calc(100vw - (2 * 10px)) // ref: layout .container padding
 
   li
     margin: 0 auto
     width: 450px // ref: <CardSubject> max-with
+    max-width: calc(100vw - (2 * 10px)) // ref: layout .container padding
     margin-bottom: 1.5em
 </style>
