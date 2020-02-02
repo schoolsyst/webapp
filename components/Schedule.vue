1e5517479078ef80274266e0bcd2bf5beb51a12d
ewen-lbh <ewen.lebiha7@gmail.com>
2020-02-02 18:26:58 +0100

    ✨ New & improved <Schedule>

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,36 +1,61 @@
 <template lang="pug">
-  //TODO: no position:absolute, consider gaps as events. Do 1 event per 
-  .schedule-wrapper(:style="{height: this.eventHeight * this.durationLongestDay + 'rem'}")
+  .schedule
     ModalAddEvent(
-      action="edit"
       v-model="editingEvent"
+      action="edit"
       @submit="patch({ modifications: editingEvent, uuid: editingEvent.uuid })"
       @delete="del({ uuid: editingEvent.uuid })"
     )
-    ul.schedule
-      li.day(v-for="events in days")
-        ul.events
-          li.event(
-            v-for="event in events" :key="event.uuid" 
-            :style="styles(event)"
-            @click="onEventClick(event)"
-            :class="{ deleted: event.deleted, [`weektype-${event.week_type.toLowerCase()}`]: bothWeeks }"
+    table
+      thead
+        tr.days
+          td.empty &nbsp;
+          td(
+            v-for="dayName in dayNames"
+            colspan="2"
+          ) {{ dayName }}
+        tr.week-types
+          td.empty &nbsp;
+          template(
+            v-for="dayName in dayNames"
           )
-            span.subject(
-              :style="{ maxWidth: `calc(${styles(event).width} - (2 * 0.75rem))` } /*ref: 0.75rem: .event{padding}*/"
-            ) {{ event.subject.name }}
-            span.room {{ event.room }}
+            td Q1
+            td Q2
+      tbody
+        tr(
+          v-for="minute in minutes"
+          :class="{ 'new-hour': minute.time.endsWith(':00') }"
+        )
+          td.time(
+            v-if="minute.time.endsWith(':00')"
+            rowspan="60"
+          ) {{ minute.time }}
+          template(v-for="event in minute.events")
+            td.event(
+              :data-weektype="event.week_type"
+              :style="cellStyles(event)",
+              :rowspan="cellRowspan(event)"
+              :colspan="cellColspan(event)"
+              @click="cellOnClick(event)"
+            )
+              .subject {{ event.subject.name }}
+              .room {{ event.room }}
+
 </template>
 
 <script>
-import { mapGetters, mapState, mapActions } from 'vuex'
-import groupBy from 'lodash.groupby'
+import { mapState, mapGetters, mapActions } from 'vuex'
 import {
-  startOfDay,
+  setSeconds,
+  setMinutes,
+  setHours,
+  format,
   startOfWeek,
   endOfWeek,
+  addMinutes,
   differenceInMinutes
 } from 'date-fns'
+import groupBy from 'lodash.groupby'
 import ModalAddEvent from '~/components/ModalAddEvent.vue'
 
 export default {
@@ -64,93 +89,114 @@ export default {
       identifier$action: 'edit'
     }
     return {
-      eventWidth: 10,
-      eventHeight: 0.125,
       editingEvent: {
         ...defaults,
         uuid: null
-      }
+      },
+      dayNames: [
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi'
+        // 'Dimanche'
+      ]
     }
   },
   computed: {
     ...mapState(['now']),
     ...mapGetters('schedule', ['coursesIn']),
-    days() {
-      const courses = this.coursesIn(
-        startOfWeek(this.now),
-        endOfWeek(this.now),
-        {
-          includeDeleted: this.showDeleted,
-          includeBothWeekTypes: this.bothWeeks
-        }
-      )
-      return groupBy(courses, 'day')
+    scheduleStart() {
+      let start = new Date()
+      start = setHours(start, 8)
+      start = setMinutes(start, 0)
+      start = setSeconds(start, 0)
+      return start
     },
-    durationLongestDay() {
-      // Get total durations for each day
-      const durations =
-        // Get the array of events for each day
-        Object.values(this.days)
-          // Get an array of total duration in minutes for each day
-          .map((events) =>
-            // Map each event to its duration in minutes
-            events
-              .map((event) => differenceInMinutes(event.end, event.start))
-              // Sum the durations of each event of each day
-              .reduce((acc, cur) => acc + cur)
-          )
-      // Get the longest duration sum
-      return Math.max(...durations)
+    scheduleEnd() {
+      let end = new Date()
+      end = setHours(end, 19)
+      end = setMinutes(end, 0)
+      end = setSeconds(end, 0)
+      return end
+    },
+    emptyMinutes() {
+      // Create an object <time>: [] with time being every minute from scheduleStart to scheduleEnd
+      const keys = []
+      let currentTime = this.scheduleStart
+      while (currentTime.getTime() <= this.scheduleEnd.getTime()) {
+        keys.push(format(currentTime, 'HH:mm'))
+        currentTime = addMinutes(currentTime, 1)
+      }
+      const minutes = {}
+      keys.forEach((time) => {
+        minutes[time] = []
+      })
+      return minutes
+    },
+    minutes() {
+      // Structure:
+      /*
+        [
+          {
+            time: <HH:mm>
+            events: <Event>[]
+          }
+        ]
+      */
+      // In the DOM:
+      /*
+      <tr>
+        <td class="hour"> {{ time.minutes == 0 ? time : "" }} </td>
+        @each events
+          <td :data-weektype="week_type"> (<Event> markup...) </td>
+     */
+      let courses = this.coursesIn(
+        startOfWeek(new Date()),
+        endOfWeek(new Date())
+      )
+      // Add a `time` prop to each course in order to group them.
+      courses = courses.map((course) => {
+        let time = course.start
+        time = format(time, 'HH:mm', new Date())
+        return { ...course, time }
+      })
+      const grouped = { ...this.emptyMinutes, ...groupBy(courses, 'time') }
+      const flatGroups = Object.keys(grouped).map((time) => {
+        return {
+          time,
+          events: grouped[time]
+        }
+      })
+      return flatGroups
     }
   },
-  async mounted() {
-    await this.$store.dispatch('schedule/load')
-  },
   methods: {
-    ...mapGetters('schedule', ['startOfDay']),
     ...mapActions('schedule', { patch: 'patchEvent', del: 'deleteEvent' }),
     ...mapGetters(['textColor']),
-    heightFromDates(left, right) {
-      return (
-        Math.abs(differenceInMinutes(left, right)) * this.eventHeight + 'rem'
-      )
-    },
-    onEventClick(event) {
-      if (!this.readOnly) {
-        const modalName = this.namespace
-          ? `${this.namespace}-edit-event`
-          : 'edit-event'
-        Object.assign(this.editingEvent, event)
-        this.$modal.show(modalName)
-      }
-      this.$emit('event-click', event)
-    },
-    styles(event) {
-      const firstCourseStart = this.startOfDay(startOfDay(event.start))
-      const thisCourseStart = event.start
-      const styles = {
-        backgroundColor: event.subject.color,
+    cellStyles(event) {
+      return {
         color: this.textColor()(event.subject.color),
-        top: this.heightFromDates(firstCourseStart, thisCourseStart),
-        left: (event.day - 1) * this.eventWidth,
-        width: this.eventWidth,
-        height: this.heightFromDates(event.start, event.end)
+        backgroundColor: event.subject.color
       }
-      // When we are showing both week types at the same time, we need to show some events side by side
-      if (this.bothWeeks && event.week_type !== 'BOTH') {
-        styles.width /= 2
-        if (event.week_type === 'Q2') {
-          styles.left += styles.width
-        }
-      }
-      // Add units
-      styles.width += 'rem'
-      styles.left += 'rem'
-      return styles
     },
-    handleInput(event) {
-      console.log(event)
-      Object.assign(this.editingEvent, event)
+    cellRowspan(event) {
+      const diff = differenceInMinutes(event.start, event.end)
+      const minutes = Math.abs(diff)
+      return minutes
+    },
+    cellColspan(event) {
+      let colspan = 2
+      if (event.week_type !== 'BOTH' && this.bothWeeks) {
+        colspan = 1
+      }
+      return colspan
+    },
+    cellOnClick(event) {
+      this.editingEvent = event
+      this.$modal.open('edit-event')
+      this.$emit('event-click', event)
     }
   }
 }
@@ -158,36 +204,44 @@ export default {
 
 <style lang="stylus" scoped>
 .schedule
-  position: relative
-  height: 100vh // TODO: use computed Schedule height
-  border-top: 2px solid var(--grey)
+  overflow-x auto
+table
+  table-layout fixed
+  width 100%
+  min-width 6 * 7rem
+  // This is essential, else the table looks like this: https://i.imgur.com/uQ74W44.png
+  border-collapse collapse
+thead
+  td
+    height 1.5rem
+    text-align center
+.week-types
+  border-bottom 2px solid var(--black)
+  td
+    color var(--grey)
 
-.day
-  border-right: 2px solid var(--grey)
+td.time
+  border-right 2px solid var(--grey)
+  height 5rem
+  font-family var(--fonts-monospace-light)
+  width 5 * 1ch + .5em
+  text-align right
+  padding 1em
+  vertical-align top
+tr.new-hour
+  min-height 1rem
+  border-bottom 2px solid var(--grey)
 
 .event
-  position: absolute
-  display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
-  padding: 0.75em
-  flex-direction: column
+  text-align center
+  padding 0.5em
   cursor pointer
-
   .room
-    position: absolute
-    bottom: 0.75em
-    right: 0.75em
     opacity: 0.75
-    font-family: var(--fonts-monospace)
-
+    font-family var(--fonts-monospace-light)
   .subject
-    white-space normal
-    overflow hidden
-    display block
+    overflow-x hidden
+    width 100%
     text-overflow ellipsis
-
-ul
-  list-style: none
+    white-space normal
 </style>
