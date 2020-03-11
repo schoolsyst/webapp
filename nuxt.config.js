72a713c8e14c61acc3d112cb2f835f87a2c8cb02
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-03-11 19:47:31 +0100

    🔧 Add config for debugging w/ VSCode

--- nuxt.config.js
+++ nuxt.config.js
@@ -191,7 +191,11 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
+    }
   },
   pwa: {
     meta: {
