848f3e1830958abbd8a72bb327146569c14128fa
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-04-11 23:39:12 +0200

    ✅ Update <BadgeSubject> tests, add tests for <CardCourse>

--- tests/components/BadgeSubject.test.js
+++ tests/components/BadgeSubject.test.js
@@ -1,12 +1,19 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import VTooltip from 'v-tooltip'
+import Vuex from 'vuex'
 import BadgeSubject from '../../components/BadgeSubject.vue'
+import { getters } from '../../store/index'
 
 const vue = createLocalVue()
 vue.directive('tooltip', VTooltip)
-const mnt = (component, opts) => mount(component, { ...opts, localVue: vue })
+vue.use(Vuex)
+const store = {
+  getters: { textColor: getters.textColor }
+}
+const mnt = (component, opts) =>
+  mount(component, { ...opts, localVue: vue, store })
 
-describe('BadgeSubject', () => {
+describe('<BadgeSubject>', () => {
   test('is a Vue instance', () => {
     const component = mnt(BadgeSubject)
     expect(component.isVueInstance()).toBeTruthy()
@@ -23,15 +30,6 @@ describe('BadgeSubject', () => {
     const component = mnt(BadgeSubject)
     expect(component.is('span')).toBeTruthy()
   })
-  // Deactivated because [error] TypeError: Cannot read property 'getters' of undefined
-  // test('text is bright when subject color is dark', () => {
-  //   const component = mount(BadgeSubject, {
-  //     propsData: {
-  //       color: '#332299'
-  //     }
-  //   })
-  //   expect(component.attributes().style.color).toBe('white')
-  // })
   describe("variant 'dot'", () => {
     test('matches snapshot', () => {
       const component = mnt(BadgeSubject, { propsData: { variant: 'dot' } })
@@ -49,28 +47,5 @@ describe('BadgeSubject', () => {
     component.trigger('click')
     expect(component.emitted('click')).toBeTruthy()
   })
+  // TODO: test [no-tooltip]
 })
-
-// test('BadgeSubject emits event when clicked', (t) => {
-//   const component = render({})
-//   component.trigger('click')
-//   t.true(component.emitted('click'))
-// })
-
-// test.beforeEach(() => {
-//   wrapper = mount(BadgeSubject, {
-//     propsData: { BadgeSubject }
-//   })
-// })
-
-// test('BadgeSubject.vue shapshow', (t) => {
-//   t.snapshot({ html: wrapper.html() })
-// })
-
-// test('BadgeSubject.vue to display BadgeSubject', (t) => {
-//   const $items = wrapper.findAll('li').wrappers
-
-//   $items.forEach(($item, index) => {
-//     t.is($item.text(), BadgeSubject[index])
-//   })
-// })
---
+++ tests/components/CardCourse.test.js
@@ -0,0 +1,58 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import VTooltip from 'v-tooltip'
+import Vuex from 'vuex'
+import CardCourse from '../../components/CardCourse.vue'
+import { getters } from '../../store/index'
+
+const vue = createLocalVue()
+vue.use(Vuex)
+vue.directive('tooltip', VTooltip)
+const mockSubject = {
+  color: 'red'
+}
+const store = {
+  getters: { textColor: getters.textColor }
+}
+const mnt = (props) =>
+  mount(CardCourse, {
+    propsData: { subject: mockSubject, ...props },
+    localVue: vue,
+    store
+  })
+
+describe('<CardCourse>', () => {
+  test('is a Vue instance', () => {
+    const CardCourse = mnt()
+    expect(CardCourse.isVueInstance()).toBeTruthy()
+  })
+
+  describe('empty', () => {
+    test('matches snapshot', () => {
+      const CardCourse = mnt({ empty: true })
+      expect(CardCourse).toMatchSnapshot()
+    })
+  })
+
+  describe('with a course', () => {
+    test('matches snapshot', () => {
+      const CardCourse = mnt({
+        room: 'L102',
+        start: new Date(0),
+        end: new Date(3600 * 1000)
+      })
+      expect(CardCourse).toMatchSnapshot()
+    })
+  })
+
+  test("Emits 'close' when expanded and clicked", () => {
+    const CardCourse = mnt({ expanded: true })
+    CardCourse.trigger('click')
+    expect(CardCourse.emitted('close')).toBeTruthy()
+  })
+
+  test("Emits 'expand' when not expanded and clicked", () => {
+    const CardCourse = mnt()
+    CardCourse.trigger('click')
+    expect(CardCourse.emitted('expand')).toBeTruthy()
+  })
+})
