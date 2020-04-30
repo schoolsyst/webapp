da0c333cf33ce5255510bb7b8c713a4e5b7abfbe
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-04-30 23:57:17 +0200

    ✅ Test <CardReport>

---
+++ tests/components/CardReport.test.js
@@ -0,0 +1,29 @@
+import { bootstrapComponentTest } from '../utils'
+import CardReport from '~/components/CardReport.vue'
+
+const { mnt } = bootstrapComponentTest(
+  CardReport,
+  {},
+  {
+    title: 'Lorem ispum dolor sit amet',
+    type: 'BUG',
+    published: new Date(),
+    github_issue: 42,
+  }
+)
+
+describe('<CardReport>', () => {
+  describe('with a linked github issue', () => {
+    test('matches snpashot', () => {
+      const CardReport = mnt()
+      expect(CardReport).toMatchSnapshot()
+    })
+  })
+
+  describe('without a linked github issue', () => {
+    test('matches snapshot', () => {
+      const CardReport = mnt({ github_issue: null, published: null })
+      expect(CardReport).toMatchSnapshot()
+    })
+  })
+})
