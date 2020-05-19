f5b9cf7f323ee1dc999501c18595ef7bf388eaa8
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-05-19 17:49:32 +0200

    💄 ^utils: Fix `lightColor` and `darkColor`, add `blackOrWhite`

--- utils/colors.js
+++ utils/colors.js
@@ -3,10 +3,13 @@ import tinycolor from 'tinycolor2'
 export const lightColor = baseColor => {
   /* see https://codepen.io/ewen-lbh/pen/dyYKWVL */
   const { h, s } = tinycolor(baseColor).toHsl()
-  return tinycolor({ h, s: 2 * s, l: 0.9 }).toHslString()
+  return tinycolor({ h, s: Math.min(2 * s, 1), l: 0.85 }).toHslString()
 }
 export const darkColor = baseColor => {
   /* see https://codepen.io/ewen-lbh/pen/dyYKWVL */
   const { h, s } = tinycolor(baseColor).toHsl()
-  return tinycolor({ h, s: 0.25 * s, l: 0.4 }).toHslString()
+  return tinycolor({ h, s: Math.min(0.25 * s, 1), l: 0.3 }).toHslString()
 }
+
+export const blackOrWhite = bgColor =>
+  tinycolor(bgColor).isLight() ? 'black' : 'white'
