9753bd04a9f9deaadf76873b3b35d16334138fdc
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-05-21 23:02:04 +0200

    🔌 <TextLink> Handle internal & external links properly

--- components/TextLink.vue
+++ components/TextLink.vue
@@ -1,7 +1,11 @@
 <template>
-  <a :href="href" class="--text-link">
+  <component
+    :is="element"
+    v-bind="{ ...toOrHref, target, rel, title, ...$attrs }"
+    :class="{ '--text-link': true, external: isExternal }"
+  >
     <slot></slot>
-  </a>
+  </component>
 </template>
 
 <script lang="ts">
@@ -10,11 +14,56 @@ import Vue, { PropOptions } from 'vue'
 import { isURL } from '~/utils/validators'
 export default Vue.extend({
   props: {
-    href: {
+    to: {
       type: String,
       required: true,
       validator: isURL,
     } as PropOptions<string>,
+    newTab: {
+      type: Boolean,
+      default: false,
+    } as PropOptions<boolean>,
+    title: {
+      type: String,
+      default: '',
+    } as PropOptions<string>,
+  },
+  computed: {
+    isExternal(): boolean {
+      /* If the URL targeted starts with one of the three protocols http, https or ftp,
+       * the link  is assumed to be external.
+       */
+      return /^(https?|ftp):\/\//.test(this.to)
+    },
+    element(): 'nuxt-link' | 'a' {
+      /* External links need normal <a> while internal ones can be
+       * optimized by <nuxt-link>.
+       */
+      return this.isExternal ? 'a' : 'nuxt-link'
+    },
+    toOrHref(): { href: string } | { to: string } {
+      // External links use [href], while <nuxt-link> uses [to]
+      if (this.isExternal) {
+        return { href: this.to }
+      }
+      return { to: this.to }
+    },
+    target(): '_blank' | null {
+      // Set target="_blank" to open in new tab
+      return this.newTab ? '_blank' : null
+    },
+    rel(): string | null {
+      let relArray: string[] = []
+      // Use rel="external" when relevant
+      if (this.isExternal) relArray = [...relArray, 'external']
+      // Prevent leaks via `Window.opener`
+      if (this.newTab) relArray = [...relArray, 'noopener', 'noreferrer']
+      // rel list is space-separated
+      const relString = relArray.join(' ')
+      // Don't include the attribute if the list is empty
+      if (!relString) return null
+      return relString
+    },
   },
 })
 </script>
@@ -43,12 +92,12 @@ export default Vue.extend({
   border-bottom 2px solid
 
 .--text-link.external::after
-  font-family: 'Material Icons'
-  content: '\e89e'
-  font-size: 1.2em
-  margin-left: 0.1em
-  position: relative
-  bottom: -.1em
+  position relative
+  bottom -0.1em
+  margin-left 0.1em
+  content '\e89e'
+  font-size 1.2em
+  font-family 'Material Icons'
 
 //
 //Colors
