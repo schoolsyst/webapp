0e19a645da0624ffdf0b06477f780a5886a8fce4
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-05-21 21:46:08 +0200

    🔌 Add `isIn` validator

--- utils/validators.js
+++ utils/validators.js
@@ -1,3 +1,4 @@
 export const isHexColor = value => /^#([0-9a-fA-F]{3}){1,2}$/.test(value)
 export const isURL = value =>
   /^(ftp|https?):\/\/([\w\d-]+\.)+\w{2,}(\/.+)?$/.test(value)
+export const isIn = allowedValues => value => allowedValues.includes(value)
