8cc2f1ea875760ecc8d26725498b4b74a240cbd2
ewen-lbh <ewen.lebihan7@gmail.com>
2020-05-27 16:18:31 +0200

    💄 <BaseInput> Add styling, allow more [element]s

--- components/BaseInput.vue
+++ components/BaseInput.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="--base-input">
-    <label :for="inputId"><slot></slot></label>
+  <div
+    :class="{
+      '--base-input': true,
+      'has-label': hasALabel,
+      focused,
+      errored: errors.length > 0,
+    }"
+  >
+    <label v-if="hasALabel" :for="inputId"><slot></slot></label>
+    <!-- <p v-if="readonly" class="readonly-input">{{ value }}</p> -->
     <component
       :is="element"
       :id="inputId"
@@ -8,9 +16,16 @@
       :name="name"
       :placeholder="placeholder"
       :aria-placeholder="placeholder"
+      :value="value"
+      :readonly="readonly"
+      :class="{ input: true, resizable: inputAttrs && inputAttrs.resizable }"
       v-bind="inputAttrs"
-    ></component>
-    <ul v-if="errors">
+      @focus="focused = true"
+      @blur="focused = false"
+      @input="$emit('input', $event.target.value)"
+      >{{ value }}</component
+    >
+    <ul v-if="errors.length > 0" class="errors">
       <li v-for="error in errors" :key="error">{{ error }}</li>
     </ul>
   </div>
@@ -19,7 +34,6 @@
 <script lang="ts">
 // eslint-disable-next-line no-unused-vars
 import Vue, { PropOptions } from 'vue'
-import { isIn } from '~/utils/validators'
 type HTMLInputType =
   | 'button'
   | 'checkbox'
@@ -44,13 +58,21 @@ type HTMLInputType =
   | 'url'
   | 'week'
 
+type HTMLInputElement =
+  | 'input'
+  | 'textarea'
+  | 'multiselect'
+  | 'progress'
+  | 'meter'
+  | 'output'
+  | 'fieldset'
+
 export default Vue.extend({
   props: {
     element: {
       type: String,
       default: 'input',
-      validator: isIn(['input', 'textarea']),
-    } as PropOptions<'input' | 'textarea'>,
+    } as PropOptions<HTMLInputElement>,
     type: {
       type: String,
       default: 'text',
@@ -79,10 +101,22 @@ export default Vue.extend({
       type: Boolean,
       default: false,
     } as PropOptions<boolean>,
+    readonly: {
+      type: Boolean,
+      default: false,
+    } as PropOptions<boolean>,
+  },
+  data() {
+    return {
+      focused: false,
+    }
   },
   computed: {
     inputId(): string {
-      return `input--${name}`
+      return `input--${this.name}`
+    },
+    hasALabel(): boolean {
+      return Boolean(this.$slots?.default?.[0]?.text)
     },
   },
 })
@@ -90,48 +124,116 @@ export default Vue.extend({
 
 <style lang="stylus" scoped>
 //
-// Definitions
+//Definitions
 //
-
-
-
-//
-// Positioning
-//
-
-
+label-passive-offset-y = 1em
+border-width = 1.5px
 
 //
-// Sizing
+//Positioning
 //
+.--base-input
+  position relative
+  display flex
+  flex-direction column
 
-
-
-//
-// Spacing
-//
-
-
-
-//
-// Decoration
-//
-
-
+label
+  position absolute
+  top -(label-passive-offset-y + 0.5)
+  left 0
 
 //
-// Colors
+//Sizing
 //
-
-
-
-//
-// Typography
-//
-
-
+.input
+  width 100%
+  height 100%
 
 //
-// States
+//Spacing
 //
+.input, .readonly-input
+  padding 0.5em
+
+//Make more vertical room for label
+.--base-input.has-label .input
+  padding 0.8em
+
+label
+  padding 0 0.5em
+
+.errors
+  margin-top 0.5em
+
+//
+//Decoration
+//
+.input
+  border border-width solid
+  border-radius var(--border-radius)
+
+//
+//Colors
+//
+.input
+  border-color var(--grey-dark)
+  color inherit
+
+.errors
+  color var(--red)
+
+//
+//Typography
+//
+label
+  text-transform uppercase
+  letter-spacing 1px
+  font-weight 500
+  font-size 0.75em
+
+.errors li::before
+  content '– '
+
+//
+//Hacks
+//
+label
+  background-color var(--white)
+
+//Make room for the label, since position:absolute doesn't make any
+.--base-input.has-label
+  margin-top label-passive-offset-y
+
+//
+//Reactions
+//
+label
+  //transition left 0.125s ease, top 0.125s ease, color 0.25s ease
+  transition all 250ms ease
+
+.--base-input:hover, .--base-input.focused
+  label
+    left 1em
+
+.--base-input.focused
+  label
+    top -0.5em
+    color var(--blue)
+
+  .input
+    border-color var(--blue)
+
+.--base-input.errored
+  label
+    color var(--red)
+
+.--base-input.errored.focused
+  .input
+    border-color var(--red)
+
+.--base-input .input[readonly]
+  border-color transparent
+
+.--base-input .input:not(.resizable)
+  resize none
 </style>
