8ff5cdfb99dd7668b4fa9d56693ded8de496929e
ewen-lbh <ewen.lebihan7@gmail.com>
2020-05-27 16:19:20 +0200

    🔌 Create <InputText>

---
+++ components/InputText.vue
@@ -0,0 +1,75 @@
+<template>
+  <base-input
+    type="text"
+    :element="block ? 'textarea' : 'input'"
+    v-bind="{ ...$props }"
+    @input="$emit('input', $event)"
+  >
+    <slot></slot>
+  </base-input>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line no-unused-vars
+import Vue, { PropOptions } from 'vue'
+import BaseInput from '~/components/BaseInput.vue'
+
+export default Vue.extend({
+  components: { BaseInput },
+  extends: BaseInput,
+  props: {
+    block: {
+      type: Boolean,
+      default: false,
+    } as PropOptions<boolean>,
+  },
+})
+</script>
+
+<style lang="stylus" scoped>
+//
+// Definitions
+//
+
+
+
+//
+// Positioning
+//
+
+
+
+//
+// Sizing
+//
+
+
+
+//
+// Spacing
+//
+
+
+
+//
+// Decoration
+//
+
+
+
+//
+// Colors
+//
+
+
+
+//
+// Typography
+//
+
+
+
+//
+// States
+//
+</style>
